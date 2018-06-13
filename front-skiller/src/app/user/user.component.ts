2c72e5cf5da23a76d97559df2e7c5fd701f0892a
Frédéric <frederic.vidal.perso@gmail.com>
2018-06-13 08:00:19 +0200

    Filling the levels select item

--- front-skiller/src/app/user/user.component.ts
+++ front-skiller/src/app/user/user.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CinematicService } from '../cinematic.service';
+import {Component, OnInit} from '@angular/core';
+import {CinematicService} from '../cinematic.service';
 import {Collaborater} from '../collaborater';
+
+import {Level} from '../data/level';
+
+import {LIST_OF_LEVELS} from '../data/List_of_levels';
 
 @Component({
   selector: 'app-user',
@@ -9,17 +13,36 @@ import {Collaborater} from '../collaborater';
 })
 export class UserComponent implements OnInit {
 
-  collaborater: Collaborater;
   
-  constructor(private cinematicService:CinematicService) {}
+  levels: Level[] = LIST_OF_LEVELS;
+  
+  
+  collaborater: Collaborater;
+
+  code: string;
+  
+  constructor(private cinematicService: CinematicService) {}
 
   ngOnInit() {
-    this.collaborater = new Collaborater(0, "Frédéric", "VIDAL", "altF4", 'frvidal@sqli.com');
+    this.collaborater = new Collaborater(0, "Frédéric", "VIDAL", "altF4", 'frvidal@sqli.com', 'ET 2');
     this.cinematicService.setForm("Welcome to a new developer !");
+    this.code = this.levels[0].code;
   }
 
-    save() : void {
-      console.log ("saving data for M." + this.collaborater.lastName);
-    }
+  save(): void {
+    console.log("saving data for M." + this.collaborater.lastName);
+  }
 
+  update = function() {
+    console.log("updating the value");
+    // $scope.item.size.code = $scope.selectedItem.code;
+  }
+
+  selectchange(args){ 
+    console.log("change");
+    console.log(args.target.value); 
+//  this.countryName = args.target.options[args.target.selectedIndex].text; 
+  } 
 }
+
+
