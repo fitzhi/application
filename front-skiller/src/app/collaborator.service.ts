753d9d6a404dad80044c3bd98435656757fbb5a6
Frédéric <frederic.vidal.perso@gmail.com>
2018-07-31 10:03:51 +0200

    CLEANUP code

--- front-skiller/src/app/collaborator.service.ts
+++ front-skiller/src/app/collaborator.service.ts
@@ -36,7 +36,9 @@ export class CollaboratorService extends InternalService {
     return this.http.get<Collaborator[]>(this.collaboratorUrl + '/all');
   }
 
-  /** GET collaborater by id. Will 404 if id not found */
+  /**
+   * GET staff member associated to this id. Will throw a 404 if id not found.
+   */
   get(id: number): Observable<Collaborator> {
     const url = this.collaboratorUrl + '/' + id;
     if (Constants.DEBUG) {
@@ -45,45 +47,33 @@ export class CollaboratorService extends InternalService {
     return this.http.get<Collaborator>(url);
   }
 
-  updateCollaborater(collaborater: Collaborator): Observable<any> {
-    return this.http.put(this.collaboratorUrl, collaborater, httpOptions).pipe(
-      tap(_ => this.log(`updated collaborater id=${collaborater.id}`)),
-      catchError(this.handleError<any>('updateCollaborater'))
-    );
-  }
-
-  /** POST: update or add a new collaborator to the server */
+  /**
+   * POST: update or add a new collaborator to the server
+   */
   save(collaborator: Collaborator): Observable<Collaborator> {
     if (Constants.DEBUG) {
       console.log('Saving the collaborator ' + collaborator.id);
     }
     return this.http.post<Collaborator>(this.collaboratorUrl + '/save', collaborator, httpOptions).pipe(
-      tap( (collab: Collaborator) =>
-        this.log('updated collaborator w/ id=${collab.id}')),
+      tap( (collab: Collaborator) => {
+          if (Constants.DEBUG) {
+            this.log('updated staff member for id ' + collab.id);
+          }
+        },
         catchError(this.handleError<Collaborator>('addCollaborater'))
-    );
+    ));
   }
 
-  /** DELETE: delete the collaborater from the server */
-  deleteCollaborater(collaborater: Collaborator | number): Observable<Collaborator> {
+  /**
+   * DELETE delete a staff member from the server
+   */
+  delete(collaborater: Collaborator | number): Observable<Collaborator> {
     const id = typeof collaborater === 'number' ? collaborater : collaborater.id;
     const url = `${this.collaboratorUrl}/${id}`;
 
     return this.http.delete<Collaborator>(url, httpOptions).pipe(
       tap(_ => this.log(`deleted collaborater id=${id}`)),
       catchError(this.handleError<Collaborator>('deleteCollaborater'))
-    );
-  }
-
-  /* GET collaborators whose name contains search term */
-  searchCollaborateres(term: string): Observable<Collaborator[]> {
-    if (!term.trim()) {
-      // if not search term, return empty collaborater array.
-      return of([]);
-    }
-    return this.http.get<Collaborator[]>(`${this.collaboratorUrl}/?name=${term}`).pipe(
-      tap(_ => this.log(`found collaborateres matching "${term}"`)),
-      catchError(this.handleError<Collaborator[]>('searchCollaborateres', []))
     );
   }
 }
