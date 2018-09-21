a110323e4a9f08edb3ec9f34ec07b215dafce272
Frédéric <frederic.vidal.perso@gmail.com>
2018-09-21 15:25:38 +0200

    [MPL] level.replace level.code

--- front-skiller/src/app/list-staff/list-staff.component.ts
+++ front-skiller/src/app/list-staff/list-staff.component.ts
@@ -2,7 +2,9 @@ import {Component, OnInit, Input} from '@angular/core';
 import {CinematicService} from '../cinematic.service';
 import {Constants} from '../constants';
 import {Collaborator} from '../data/collaborator';
+import { Profile } from '../data/profile';
 import { ListStaffService } from '../list-staff-service/list-staff.service';
+import { ReferentialService } from '../referential.service';
 
 @Component({
   selector: 'app-list-staff',
@@ -13,13 +15,27 @@ export class ListStaffComponent implements OnInit {
 
   private collaborators: Collaborator[];
 
+  private profiles: Profile[];
+
   constructor(
     private cinematicService: CinematicService,
+    private referentialService: ReferentialService,
     private listStaffService: ListStaffService) {}
 
   ngOnInit() {
     this.cinematicService.setForm(Constants.DEVELOPPERS_SEARCH);
     this.collaborators = this.listStaffService.getStaff();
+
+    this.referentialService.behaviorSubjectProfiles.subscribe(
+      (profiles: Profile[]) => this.profiles = profiles);
   }
 
+  levelTitle (code: String): String {
+    const found = this.profiles.find(profile => (profile.code === code));
+    if (typeof found === 'undefined') {
+      return '';
+    } else {
+      return found.title;
+    }
+  }
 }
