0090eebfad03efd44dacd5b80396aa8350a938bb
Frédéric <frederic.vidal.perso@gmail.com>
2018-09-26 09:14:56 +0200

    [IMPL] JUnit test for countAllStaff_GroupBy_Skill_Level

--- back-skiller/src/test/java/fr/skiller/bean/impl/StaffHandler_Skill_Test.java
+++ back-skiller/src/test/java/fr/skiller/bean/impl/StaffHandler_Skill_Test.java
@@ -3,34 +3,21 @@
  */
 package fr.skiller.bean.impl;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import fr.skiller.bean.StaffHandler;
 import fr.skiller.data.internal.PeopleCountExperienceMap;
 import fr.skiller.data.internal.Experience;
 import fr.skiller.data.internal.Staff;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Fr&eacute;d&eacute;ric VIDAL
@@ -50,8 +37,35 @@ public class StaffHandler_Skill_Test {
 	@Test
 	public void countAllStaffGroupBySkill_Level() {
 		
+		Staff s1 = new Staff(10001, "firstName 1", "lastName 1", "nickName 1", "login 1", "email 1", "ICD 1");
+		s1.experiences.add(new Experience (10000, "test", 3));
+		
+		Staff s2 = new Staff(10002, "firstName 2", "lastName 2", "nickName 2", "login 2", "email 2", "ICD 2");
+		s2.experiences.add(new Experience (10000, "test", 3));
+
+		Staff s3 = new Staff(10003, "firstName 3", "lastName 3", "nickName 3", "login 3", "email 3", "ICD 3");
+		s3.experiences.add(new Experience (10000, "test", 5));
+		
+		staffHandler.put(10001, s1);
+		staffHandler.put(10002, s2);
+		staffHandler.put(10003, s3);
 		
 		PeopleCountExperienceMap results = staffHandler.countAllStaff_GroupBy_Skill_Level(true);
+		Assert.assertTrue(results.get("10000-1") == null);
+		Assert.assertTrue(results.get("10000-3") == 2);
+		Assert.assertTrue(results.get("10000-5") == 1);
+		
+		s2.isActive = false;
+
+		results = staffHandler.countAllStaff_GroupBy_Skill_Level(true);
+		Assert.assertTrue(results.get("10000-1") == null);
+		Assert.assertTrue(results.get("10000-3") == 1);
+		Assert.assertTrue(results.get("10000-5") == 1);
+		
+		results = staffHandler.countAllStaff_GroupBy_Skill_Level(false);
+		Assert.assertTrue(results.get("10000-1") == null);
+		Assert.assertTrue(results.get("10000-3") == 2);
+		Assert.assertTrue(results.get("10000-5") == 1);
 		
 		staffHandler.init();
 	}
