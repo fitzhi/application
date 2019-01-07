189e1a35115b95da6be84550f737a76ceb9fc666
Frédéric <frederic.vidal.perso@gmail.com>
2019-01-07 21:27:09 +0100

    [IMPL] Project staff team tab pane
    Implementation with some tests data

--- front-skiller/src/app/project/project-staff/project-staff.component.ts
+++ front-skiller/src/app/project/project-staff/project-staff.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Contributor {
+  firstName: string;
+  lastName: string;
+  login: string;
+  lastCommit: string;
+  numberOfCommits: number;
+  numberOfFiles: number;
+}
+
+/*
+* Data store associated with the project staff team
+*/
+const TESTING_DATA: Contributor[] = [
+  { firstName: 'jean', lastName: 'Dupont', login: 'jdupont', lastCommit: '15/12/2018 18:00', numberOfCommits: 15, numberOfFiles: 25 },
+  { firstName: 'isidore', lastName: 'Dupond', login: 'idupond', lastCommit: '20/12/2018 18:00', numberOfCommits: 200, numberOfFiles: 328 },
+];
+
 @Component({
   selector: 'app-project-staff',
   templateUrl: './project-staff.component.html',
   styleUrls: ['./project-staff.component.css']
 })
 export class ProjectStaffComponent implements OnInit {
+
+  public dataSource = TESTING_DATA;
+
+  public  displayedColumns: string[] = ['name', 'login', 'lastCommit', 'numberOfCommits', 'numberOfFiles'];
 
   constructor() { }
 
