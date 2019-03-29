815ee445cf7bfee15ca03c93ad94b26fb51dbcbe
frvidal <frederic.vidal.perso@gmail.com>
2019-03-29 11:48:13 +0100

    [FIX] Install & first launch

---
+++ front-skiller/init.sh
@@ -0,0 +1,9 @@
+npm install
+npm install npm -g
+npm install --save-dev @angular/cli@latest
+npm install rxjs@6 rxjs-compat@6 --save
+npm install jquery@3.3.1 popper.js@^1.14.3 bootstrap@4.1.1 --save
+npm install @angular/forms@6.0.1 bootstrap ngx-bootstrap --save
+npm install @ng-bootstrap/ng-bootstrap --save
+npm install --save @angular/material @angular/cdk @angular/animations
+
