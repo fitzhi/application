06b66a117f03d5ab0278e4c5c5674c8ce2bc194e
frvidal <frederic.vidal.perso@gmail.com>
2019-06-29 17:00:45 +0200

    [IMPL][Front] Test to prevent an existing staff member.

--- front-skiller/src/app/message/message.service.ts
+++ front-skiller/src/app/message/message.service.ts
@@ -1,46 +1,46 @@
-import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs/Subject';
-import {Message} from './message';
-import {Constants} from '../constants';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Message } from './message';
+import { Constants } from '../constants';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class MessageService {
 
-  myMessage: Subject<Message> = new Subject<Message>();
+	myMessage: Subject<Message> = new Subject<Message>();
 
-  newMessage$ = this.myMessage.asObservable();
+	newMessage$ = this.myMessage.asObservable();
 
-  constructor() {
-  }
+	constructor() {
+	}
 
-  /*
-  * set a new message
-  */
-  public set(gravity: number, message: string) {
-    this.myMessage.next(new Message(gravity, message));
-    setTimeout(() => this.myMessage.next(new Message(Constants.MESSAGE_VOID, '')), 5000);
-  }
+	/*
+	* set a new message
+	*/
+	public set(gravity: number, message: string) {
+		this.myMessage.next(new Message(gravity, message));
+		setTimeout(() => this.myMessage.next(new Message(Constants.MESSAGE_VOID, '')), 5000);
+	}
 
-  /*
-  * set a new ERROR message
-  */
-  public error(message: string) {
-    this.set(Constants.MESSAGE_ERROR, message);
-  }
+	/*
+	* set a new ERROR message
+	*/
+	public error(message: string) {
+		this.set(Constants.MESSAGE_ERROR, message);
+	}
 
-  /*
-  * display an snack info message.
-  */
-  public warning(message: string) {
-    this.set(Constants.MESSAGE_WARNING, message);
-  }
+	/*
+	* display an snack info message.
+	*/
+	public warning(message: string) {
+		this.set(Constants.MESSAGE_WARNING, message);
+	}
 
-  /*
-  * display an snack info message.
-  */
-  public info(message: string) {
-    this.set(Constants.MESSAGE_INFO, message);
-  }
+	/*
+	* display an snack info message.
+	*/
+	public info(message: string) {
+		this.set(Constants.MESSAGE_INFO, message);
+	}
 }
--- front-skiller/src/app/tabs-staff/staff-form/staff-form.component.ts
+++ front-skiller/src/app/tabs-staff/staff-form/staff-form.component.ts
@@ -7,222 +7,267 @@ import { Profile } from '../../data/profile';
 import { ReferentialService } from '../../service/referential.service';
 import { StaffDataExchangeService } from '../service/staff-data-exchange.service';
 
-import { Component, OnInit, Input, OnDestroy, Output, EventEmitter, Injector } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, Output, EventEmitter } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BaseComponent } from '../../base/base.component';
 import { TabsStaffListService } from 'src/app/tabs-staff-list/service/tabs-staff-list.service';
 import { take } from 'rxjs/operators';
+import { MessageBoxService } from 'src/app/message-box/service/message-box.service';
 
 
 @Component({
-    selector: 'app-staff-form',
-    templateUrl: './staff-form.component.html',
-    styleUrls: ['./staff-form.component.css']
+	selector: 'app-staff-form',
+	templateUrl: './staff-form.component.html',
+	styleUrls: ['./staff-form.component.css']
 })
 export class StaffFormComponent extends BaseComponent implements OnInit, OnDestroy {
 
-    @Input('idStaff')
-    private idStaff: number;
+	@Input('idStaff')
+	private idStaff: number;
 
-    /**
+	FIRST_NAME = 1;
+	LAST_NAME = 2;
+
+	/**
      * This messenger is there to be used in one case : if the parent component is the startingSetup.
      * The staff member has been updated during the first start process.
      * We can continue to next step.
      */
-    @Output() messengerStaffUpdated = new EventEmitter<Collaborator>();
+	@Output() messengerStaffUpdated = new EventEmitter<Collaborator>();
 
-    private collaborator: Collaborator;
+	private collaborator: Collaborator;
 
-    /**
+	/**
      * Label on side of the active check box.
      */
-    label_isActive: string;
-    label_dateInactive: Date;
+	label_isActive: string;
+	label_dateInactive: Date;
 
-    /**
+	/**
      * list of profiles used on the SELECT field.
      */
-    profiles: Profile[];
+	profiles: Profile[];
 
-    public profileStaff = new FormGroup({
-        firstName: new FormControl('', [Validators.maxLength(16)]),
-        lastName: new FormControl('', [Validators.required, Validators.maxLength(16)]),
-        nickName: new FormControl('', [Validators.maxLength(16)]),
-        login: new FormControl('', [Validators.required, Validators.maxLength(16)]),
-        email: new FormControl('', [Validators.required, Validators.maxLength(32), Validators.email]),
-        profile: new FormControl(null, [Validators.required]),
-        active: new FormControl(1),
-        external: new FormControl(0)
-    });
+	public profileStaff = new FormGroup({
+		firstName: new FormControl('', [Validators.maxLength(16)]),
+		lastName: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+		nickName: new FormControl('', [Validators.maxLength(16)]),
+		login: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+		email: new FormControl('', [Validators.required, Validators.maxLength(32), Validators.email]),
+		profile: new FormControl(null, [Validators.required]),
+		active: new FormControl(1),
+		external: new FormControl(0)
+	});
 
-    constructor(
-        private staffService: StaffService,
-        private messageService: MessageService,
-        private cinematicService: CinematicService,
-        private referentialService: ReferentialService,
-        private staffDataExchangeService: StaffDataExchangeService,
-        private tabsStaffListService: TabsStaffListService,
-        private router: Router) {
-        super();
-    }
+	constructor(
+		private staffService: StaffService,
+		private messageService: MessageService,
+		private messageBoxService: MessageBoxService,
+		private cinematicService: CinematicService,
+		private referentialService: ReferentialService,
+		private staffDataExchangeService: StaffDataExchangeService,
+		private tabsStaffListService: TabsStaffListService,
+		private router: Router) {
+		super();
+	}
 
-    ngOnInit() {
+	ngOnInit() {
 
-        if (Constants.DEBUG) {
-            console.log('Current staff member id ' + this.idStaff);
-        }
-        if ((typeof this.idStaff === 'undefined') || (this.idStaff === null)) {
-            this.collaborator = {
-                idStaff: -1, firstName: '', lastName: '', nickName: '', login: '', email: '', level: '',
-                dateInactive: null, application: '', typeOfApplication: null, active: true, external: true,
-                experiences: [], missions: []
-            };
-            this.profileStaff.get('firstName').setValue('');
-            this.profileStaff.get('lastName').setValue('');
-            this.profileStaff.get('nickName').setValue('');
-            this.profileStaff.get('login').setValue('');
-            this.profileStaff.get('email').setValue('');
-            this.profileStaff.get('profile').setValue('');
-            this.profileStaff.get('active').setValue(true);
-            this.label_isActive = 'will be considered in activity as long as this box is checked ';
-            this.profileStaff.get('external').setValue(false);
-        } else {
-            /**
+		if (Constants.DEBUG) {
+			console.log('Current staff member id ' + this.idStaff);
+		}
+		if (!this.idStaff) {
+			this.initStaff()
+;		} else {
+			/**
              * We listen the parent component (StaffComponent) in charge of retrieving data from the back-end.
              */
-            this.subscriptions.add(
-                this.staffDataExchangeService.collaboratorObserver
-                    .subscribe((employee: Collaborator) => {
-                        if (Constants.DEBUG) {
-                            console.log('Employee loaded ' + employee.idStaff);
-                        }
-                        this.collaborator = employee;
-                        this.profileStaff.get('firstName').setValue(this.collaborator.firstName);
-                        this.profileStaff.get('lastName').setValue(this.collaborator.lastName);
-                        this.profileStaff.get('nickName').setValue(this.collaborator.nickName);
-                        this.profileStaff.get('login').setValue(this.collaborator.login);
-                        this.profileStaff.get('email').setValue(this.collaborator.email);
-                        this.profileStaff.get('profile').setValue(this.collaborator.level);
-                        this.profileStaff.get('active').setValue(this.collaborator.active);
-                        if (this.collaborator.active) {
-                            if (this.collaborator.idStaff === null) {
-                                this.label_isActive = 'will be considered in activity as long as this box is checked ';
-                            } else {
-                                if (this.collaborator.lastName === null) {
-                                    this.label_isActive =
-                                    'This collaborator is still in activity. Uncheck this box to inform of his leave.';
-                                } else {
-                                    this.label_isActive =
-                                        ((this.collaborator.firstName === null) ? '' : this.collaborator.firstName)
-                                        + ' ' + this.collaborator.lastName
-                                        + ' is still in activity. Uncheck this box to inform of his leave.';
-                                }
-                            }
-                            // There is no READONLY attribute in the SELECT widget.
-                            // We need to enable this field within the code and not in HTML like the rest of the form.
-                            this.profileStaff.get('profile').enable();
-                            this.profileStaff.get('external').enable();
-                        } else {
-                            this.label_isActive = this.collaborator.firstName + ' ' +
-                                this.collaborator.lastName + ' in no more in activity since ';
-                            this.label_dateInactive = this.collaborator.dateInactive;
-                            // There is no READONLY attribute in the SELECT widget.
-                            // We need to disable this field within the code and not in HTML like the rest of the form.
-                            this.profileStaff.get('profile').disable();
-                            this.profileStaff.get('external').disable();
-                        }
-                        this.profileStaff.get('external').setValue(this.collaborator.external);
-                        this.cinematicService.setForm(Constants.DEVELOPERS_CRUD, this.router.url);
-                    }));
-        }
+			this.subscriptions.add(
+				this.staffDataExchangeService.collaboratorObserver
+					.subscribe((employee: Collaborator) => {
+						if (Constants.DEBUG) {
+							console.log('Employee loaded ' + employee.idStaff);
+						}
+						this.collaborator = employee;
+						this.profileStaff.get('firstName').setValue(this.collaborator.firstName);
+						this.profileStaff.get('lastName').setValue(this.collaborator.lastName);
+						this.profileStaff.get('nickName').setValue(this.collaborator.nickName);
+						this.profileStaff.get('login').setValue(this.collaborator.login);
+						this.profileStaff.get('email').setValue(this.collaborator.email);
+						this.profileStaff.get('profile').setValue(this.collaborator.level);
+						this.profileStaff.get('active').setValue(this.collaborator.active);
+						if (this.collaborator.active) {
+							if (this.collaborator.idStaff === null) {
+								this.label_isActive = 'will be considered in activity as long as this box is checked ';
+							} else {
+								if (this.collaborator.lastName === null) {
+									this.label_isActive =
+										'This collaborator is still in activity. Uncheck this box to inform of his leave.';
+								} else {
+									this.label_isActive =
+										((this.collaborator.firstName === null) ? '' : this.collaborator.firstName)
+										+ ' ' + this.collaborator.lastName
+										+ ' is still in activity. Uncheck this box to inform of his leave.';
+								}
+							}
+							// There is no READONLY attribute in the SELECT widget.
+							// We need to enable this field within the code and not in HTML like the rest of the form.
+							this.profileStaff.get('profile').enable();
+							this.profileStaff.get('external').enable();
+						} else {
+							this.label_isActive = this.collaborator.firstName + ' ' +
+								this.collaborator.lastName + ' in no more in activity since ';
+							this.label_dateInactive = this.collaborator.dateInactive;
+							// There is no READONLY attribute in the SELECT widget.
+							// We need to disable this field within the code and not in HTML like the rest of the form.
+							this.profileStaff.get('profile').disable();
+							this.profileStaff.get('external').disable();
+						}
+						this.profileStaff.get('external').setValue(this.collaborator.external);
+						this.cinematicService.setForm(Constants.DEVELOPERS_CRUD, this.router.url);
+					}));
+		}
 
-        this.profiles = this.referentialService.profiles;
+		this.profiles = this.referentialService.profiles;
 
-    }
+	}
 
-    /**
+	/**
+	 * Initialization of the collaborator inside this form.
+	 */
+	initStaff() {
+		this.collaborator = {
+			idStaff: -1, firstName: '', lastName: '', nickName: '', login: '', email: '', level: '',
+			dateInactive: null, application: '', typeOfApplication: null, active: true, external: true,
+			experiences: [], missions: []
+		};
+		this.profileStaff.get('firstName').setValue('');
+		this.profileStaff.get('lastName').setValue('');
+		this.profileStaff.get('nickName').setValue('');
+		this.profileStaff.get('login').setValue('');
+		this.profileStaff.get('email').setValue('');
+		this.profileStaff.get('profile').setValue('');
+		this.profileStaff.get('active').setValue(true);
+		this.label_isActive = 'will be considered in activity as long as this box is checked ';
+		this.profileStaff.get('external').setValue(false);
+	}
+
+	/**
     * The Submit Button has been activated
     */
-    onSubmit(): void {
-        if (Constants.DEBUG) {
-            console.log('Saving data for the collaborator below');
-            console.log(this.collaborator);
-        }
-        this.collaborator.firstName = this.profileStaff.get('firstName').value;
-        this.collaborator.lastName = this.profileStaff.get('lastName').value;
-        this.collaborator.nickName = this.profileStaff.get('nickName').value;
-        this.collaborator.login = this.profileStaff.get('login').value;
-        this.collaborator.email = this.profileStaff.get('email').value;
-        this.collaborator.level = this.profileStaff.get('profile').value;
-        this.collaborator.active = this.profileStaff.get('active').value;
-        this.collaborator.external = this.profileStaff.get('external').value;
+	onSubmit(): void {
+		if (Constants.DEBUG) {
+			console.log('Saving data for the collaborator below');
+			console.log(this.collaborator);
+		}
+		this.collaborator.firstName = this.profileStaff.get('firstName').value;
+		this.collaborator.lastName = this.profileStaff.get('lastName').value;
+		this.collaborator.nickName = this.profileStaff.get('nickName').value;
+		this.collaborator.login = this.profileStaff.get('login').value;
+		this.collaborator.email = this.profileStaff.get('email').value;
+		this.collaborator.level = this.profileStaff.get('profile').value;
+		this.collaborator.active = this.profileStaff.get('active').value;
+		this.collaborator.external = this.profileStaff.get('external').value;
 
-        this.staffService.save(this.collaborator)
-        .pipe(take(1))
-        .subscribe(
-            staff => {
-                this.collaborator = staff;
-                this.messengerStaffUpdated.emit (staff);
+		this.staffService.save(this.collaborator)
+			.pipe(take(1))
+			.subscribe(
+				staff => {
+					this.collaborator = staff;
+					this.messengerStaffUpdated.emit(staff);
 
-                /**
-                 * If this staff member exists in pre-existing list of collaborators. We actualize the content.
-                 */
-                this.tabsStaffListService.actualizeCollaborator(staff);
-                this.messageService.info('Staff member ' + this.collaborator.firstName + ' ' + this.collaborator.lastName + ' saved');
-            });
-    }
+					/**
+					 * If this staff member exists in pre-existing list of collaborators. We actualize the content.
+					 */
+					this.tabsStaffListService.actualizeCollaborator(staff);
+					this.messageService.info('Staff member ' + this.collaborator.firstName + ' ' + this.collaborator.lastName + ' saved');
+				});
+	}
 
-    /**
+	/**
      * Test if the collaborator has been already deactivated on the database.
      * You can test this state by testing the dateInactive, filled by the back-end during the deactivation process.
      */
-    public isAlreadyDeactived(): boolean {
-        return (!this.collaborator.active);
-    }
+	public isAlreadyDeactived(): boolean {
+		return (!this.collaborator.active);
+	}
 
-    /**
+	/**
      * Class of the button corresponding to the 3 possible states of the "Ok" button.
      */
-    classOkButton() {
-        return (this.profileStaff.invalid) ?
-            'okButton okButtonInvalid' : 'okButton okButtonValid';
-    }
+	classOkButton() {
+		return (this.profileStaff.invalid) ?
+			'okButton okButtonInvalid' : 'okButton okButtonValid';
+	}
 
-    get firstName(): any {
-        return this.profileStaff.get('firstName');
-    }
+	public onChange(field: number) {
+		if (this.collaborator.idStaff === -1){
+			return;
+		}
 
-    get lastName(): any {
-        return this.profileStaff.get('lastName');
-    }
+		const newFirstName = this.profileStaff.get('firstName').value;
+		const oldFirstName = this.collaborator.firstName;
+		const newLastName = this.profileStaff.get('lastName').value;
+		const oldLastName = this.collaborator.lastName;
 
-    get nickName(): any {
-        return this.profileStaff.get('nickName');
-    }
+		if (Constants.DEBUG) {
+			console.log ('Field changed', field);
+			console.log ('Firstname', newFirstName + ' ' + oldFirstName);
+			console.log ('Lastname', newLastName + ' ' + oldLastName);
+		}
 
-    get login(): any {
-        return this.profileStaff.get('login');
-    }
+		if ( (newFirstName !== oldFirstName) && (newLastName !== oldLastName) ) {
 
-    get profile(): any {
-        return this.profileStaff.get('profile');
-    }
+			this.messageBoxService.question(
+				'Staff Form',
+				'You have already changed the first and the last name of ' + oldFirstName + ' ' + oldLastName
+				+ '<br/>Click \'Yes\' if you want to create a new staff.<br/>\'No\' to continue updating this one.')
+				.pipe(take(1))
+				.subscribe(answer => {
+					if (answer) {
+						this.initStaff();
+						this.profileStaff.get('firstName').setValue(newFirstName);
+						this.profileStaff.get('lastName').setValue(newLastName);
+					}
+				});
+		}
+	}
 
-    get email(): any {
-        return this.profileStaff.get('email');
-    }
+	get firstName(): any {
+		return this.profileStaff.get('firstName');
+	}
 
-    get external(): any {
-        return this.profileStaff.get('external');
-    }
+	get lastName(): any {
+		return this.profileStaff.get('lastName');
+	}
 
-    /**
+	get nickName(): any {
+		return this.profileStaff.get('nickName');
+	}
+
+	get login(): any {
+		return this.profileStaff.get('login');
+	}
+
+	get profile(): any {
+		return this.profileStaff.get('profile');
+	}
+
+	get email(): any {
+		return this.profileStaff.get('email');
+	}
+
+	get external(): any {
+		return this.profileStaff.get('external');
+	}
+
+	/**
      * Calling the base class to unsubscribe all subscriptions.
      */
-    ngOnDestroy() {
-        super.ngOnDestroy();
-    }
+	ngOnDestroy() {
+		super.ngOnDestroy();
+	}
 
 }
