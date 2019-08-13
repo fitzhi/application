9b170979baa1651c37b10430a205b84b5b0fe24f
frvidal <frederic.vidal.perso@gmail.com>
2019-08-13 22:37:01 +0200

    [DEPLOY] update of init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,13 @@ echo ""
 echo "Building back-end Wibkac"
 echo "------------------------"
 echo ""
+mkdir -pv git_repo_for_test
+cd git_repo_for_test
+rm -Rf first-test
+git clone https://github.com/frvidal/first-test 
+rm -Rf wibkac
+git clone https://github.com/frvidal/wibkac
+cd ..
 mkdir -pv deploy/backend-wibkac
 cd back-wibkac
 mvn package
