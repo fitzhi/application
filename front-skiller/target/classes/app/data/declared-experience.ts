baddf8cd0a0fbeb04b942d43959d0650705484b6
frvidal <frederic.vidal.perso@gmail.com>
2019-08-27 09:45:25 +0200

    [Fix][Front] remove of the /target directory

--- front-skiller/target/classes/app/data/declared-experience.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This is a skill with the skill level for a staff member
- */
-export class DeclaredExperience {
-	public idSkill: number;
-	public title: string;
-	public times: number;
-}
--- front-skiller/target/classes/app/data/experience.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This is a skill with the skill level for a staff member
- */
-export class Experience {
-	public id: number;
-	public title: string;
-	public level: number;
-}
