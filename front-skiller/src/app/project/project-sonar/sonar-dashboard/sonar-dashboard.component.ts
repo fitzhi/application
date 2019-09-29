3841519a71e62af485e9de3632e18a30bc451699
frvidal <frederic.vidal.perso@gmail.com>
2019-09-29 11:25:57 +0200

    [IMPL][Front] Conditional DIV depending on the accessibility of Sonar & its version

--- front-skiller/src/app/project/project-sonar/sonar-dashboard/sonar-dashboard.component.ts
+++ front-skiller/src/app/project/project-sonar/sonar-dashboard/sonar-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { Project } from 'src/app/data/project';
 import { BaseComponent } from 'src/app/base/base.component';
+import { SonarService } from 'src/app/service/sonar.service';
+import { Constants } from 'src/app/constants';
 
 @Component({
 	selector: 'app-sonar-dashboard',
@@ -14,16 +16,37 @@ export class SonarDashboardComponent extends BaseComponent implements OnInit, On
 	*/
 	@Input() project$;
 
-
 	private project = new Project();
 
-	constructor() { super(); }
+	constructor(private sonarService: SonarService) { super(); }
+
+	private isSonarAccessible = false;
+
+	/**
+	 * Equal to TRUE if the current Sonar version is 7.x or higher
+	 */
+	private isSonarVersion7x = false;
 
 	ngOnInit() {
 		this.subscriptions.add(
 			this.project$.subscribe(project => {
 				this.project = project;
 			}));
+		this.subscriptions.add(
+			this.sonarService.sonarIsAccessible$.subscribe(isSonarAccessible => {
+				if (isSonarAccessible) {
+					this.isSonarAccessible = isSonarAccessible;
+					this.isSonarVersion7x = (this.sonarService.sonarVersion.substring(0, 1) === '7');
+					if (Constants.DEBUG) {
+						if (this.isSonarVersion7x) {
+							console.log('Sonar version 7.x');
+						} else {
+							console.log('Sonar version < 7.x');
+						}
+					}
+				}
+
+		}));
 
 	}
 
