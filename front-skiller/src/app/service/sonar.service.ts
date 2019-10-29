a58a598d6ca7b7610493f5a03437d8b93950837d
frvidal <frederic.vidal.perso@gmail.com>
2019-10-29 21:45:14 +0100

    [IMPL][Tweak] Sonar service

--- front-skiller/src/app/service/sonar.service.ts
+++ front-skiller/src/app/service/sonar.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Settings } from '../data/settings';
-import { switchMap, map, catchError, take, tap } from 'rxjs/operators';
+import { switchMap, map, catchError, take, tap, retry } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { InternalService } from '../internal-service';
 import { BackendSetupService } from './backend-setup/backend-setup.service';
@@ -125,23 +125,23 @@ export class SonarService extends InternalService {
 
 	loadSonarSupportedMetrics() {
 		this.referentialService.supportedMetrics$.subscribe(
-				supported => this.loadSonarMetrics(supported));
+			supported => this.loadSonarMetrics(supported));
 	}
 
 	loadSonarMetrics(supported: string[]) {
 		this.httpClient.get<Metrics>(this.urlSonar + '/api/metrics/search?ps=500')
 			.pipe(
-				tap (
+				tap(
 					metrics => {
 						if (Constants.DEBUG) {
 							console.groupCollapsed(metrics.metrics.length + ' (all) metrics available on Sonar');
-							metrics.metrics.forEach(metric => console.log (metric.key, metric.name));
+							metrics.metrics.forEach(metric => console.log(metric.key, metric.name));
 							console.groupEnd();
 						}
 					}),
 				take(1))
 			.subscribe(metrics => {
-				const sonarMetrics: Metric[]  = [];
+				const sonarMetrics: Metric[] = [];
 				metrics.metrics.forEach(element => {
 					if (supported.includes(element.key)) {
 						sonarMetrics.push(element);
@@ -149,7 +149,7 @@ export class SonarService extends InternalService {
 				});
 				if (Constants.DEBUG) {
 					console.groupCollapsed(sonarMetrics.length + ' supported metrics by the application');
-					sonarMetrics.forEach(metric => console.log (metric.key, metric.name));
+					sonarMetrics.forEach(metric => console.log(metric.key, metric.name));
 					console.groupEnd();
 				}
 				this.sonarMetrics$.next(sonarMetrics);
@@ -165,65 +165,67 @@ export class SonarService extends InternalService {
 		const params = new HttpParams().set('component', key).set('metricKeys', metrics.join(','));
 		const apiMesures = '/api/measures/component';
 		return this.httpClient
-			.get<ResponseComponentMeasures>(this.urlSonar + apiMesures, {params: params})
+			.get<ResponseComponentMeasures>(this.urlSonar + apiMesures, { params: params })
 			.pipe(
-				tap ( response => {
+				tap(response => {
 					if (Constants.DEBUG) {
 						console.groupCollapsed(response.component.measures.length + ' measures obtained for component ' + response.component.key);
-						response.component.measures.forEach(measure => console.log (measure.metric, measure.value));
+						response.component.measures.forEach(measure => console.log(measure.metric, measure.value));
 						console.groupEnd();
 					}
-			}));
+				}));
 	}
 
 	loadSonarVersion() {
 		this.httpClient
 			.get(this.backendSetupService.url() + '/admin/settings')
-				.pipe(switchMap( (settings: Settings) => {
+			.pipe(
+				switchMap((settings: Settings) => {
 					return this.httpClient
 						.get(settings.urlSonar + '/api/server/version',
-								{responseType: 'text' as 'json'})
-						.pipe (
-							map( (version: string) => {
+							{ responseType: 'text' as 'json' })
+						.pipe(
+							map((version: string) => {
 								return {
 									settings: settings,
 									sonarOn: true,
 									version: version
 								};
 							}),
-							catchError( (error)  => {
-								console.log ('error', error);
-								return of( {settings: settings, sonarOn: false, version: ''});
+							catchError((error) => {
+								console.log('error', error);
+								return of({ settings: settings, sonarOn: false, version: '' });
 							})
-					); }))
-				.subscribe(
-					(data: any) => {
-						this.sonarVersion = data.version;
-						this.urlSonar =  data.settings.urlSonar;
-						this.sonarOn = data.sonarOn;
-						this.sonarIsAccessible$.next(this.sonarOn);
-						if (this.sonarOn) {
-							console.log('Sonar version ' + data.version + ' installed at the URL ' + data.settings.urlSonar);
-						} else {
-							console.log('Sonar is OFFLINE  at the URL ' + data.settings.urlSonar);
-						}
-					},
-					error => console.log (error)
-				);
+						);
+				}))
+			.subscribe(
+				(data: any) => {
+					this.sonarVersion = data.version;
+					this.urlSonar = data.settings.urlSonar;
+					this.sonarOn = data.sonarOn;
+					this.sonarIsAccessible$.next(this.sonarOn);
+					if (this.sonarOn) {
+						console.log('Sonar version ' + data.version + ' installed at the URL ' + data.settings.urlSonar);
+					} else {
+						console.log('Sonar is OFFLINE  at the URL ' + data.settings.urlSonar);
+					}
+				},
+				error => console.log(error)
+			);
 	}
 
 	/**
 	 * Load the projects declared on the Sonar instance.
 	 */
-	loadProjects () {
-		this.loadComponents('TRK').subscribe ( components => {
-				if (Constants.DEBUG) {
-					console.groupCollapsed(components.components.length + ' components retrieved.');
-					components.components.forEach(component => console.log (component.name, component.key));
-					console.groupEnd();
-				}
-				this.allSonarProjects = components.components;
-				this.allSonarProjects$.next(components.components);
+	loadProjects() {
+		this.loadComponents('TRK').subscribe(components => {
+			if (Constants.DEBUG) {
+				console.groupCollapsed(components.components.length + ' components retrieved.');
+				components.components.forEach(component => console.log(component.name, component.key));
+				console.groupEnd();
+			}
+			this.allSonarProjects = components.components;
+			this.allSonarProjects$.next(components.components);
 		});
 	}
 
@@ -231,10 +233,10 @@ export class SonarService extends InternalService {
 	 * Load the components filtered on a passed type.
 	 * @param type the given type.
 	 */
-	loadComponents (type: string): Observable<Components> {
+	loadComponents(type: string): Observable<Components> {
 		const params = new HttpParams().set('qualifiers', type).set('ps', '500');
 		return this.httpClient
-			.get<Components>(this.urlSonar + '/api/components/search', {params})
+			.get<Components>(this.urlSonar + '/api/components/search', { params })
 			.pipe(take(1));
 	}
 
@@ -242,12 +244,12 @@ export class SonarService extends InternalService {
 	 * Search the sonar project
 	 * @param sonarProject the Sonar project name
 	 */
-	search (nameOfSonarProject: string): Component {
+	search(nameOfSonarProject: string): Component {
 		if (this.allSonarProjects.length === 0) {
 			console.error('the array containing all projects declared in Sonar is empty');
 			return undefined;
 		}
-		return this.allSonarProjects.find (sp => sp.name === nameOfSonarProject);
+		return this.allSonarProjects.find(sp => sp.name === nameOfSonarProject);
 	}
 
 	/**
@@ -259,44 +261,44 @@ export class SonarService extends InternalService {
 		const params = new HttpParams().set('metric', metric).set('project', key);
 		return this.httpClient
 			.get<string>(this.urlSonar + '/api/project_badges/measure',
-				{params: params, responseType: 'text' as 'json' });
+				{ params: params, responseType: 'text' as 'json' });
 	}
 
 	loadFiles(key: string): Observable<ILanguageCount> {
 		const params = new HttpParams().set('component', key).set('qualifiers', 'FIL').set('ps', '500');
 		return this.httpClient
-			.get<ComponentTree>(this.urlSonar + '/api/components/tree', {params: params})
+			.get<ComponentTree>(this.urlSonar + '/api/components/tree', { params: params })
 			.pipe(
-				tap ( (response: ComponentTree) => {
+				tap((response: ComponentTree) => {
 					if (Constants.DEBUG) {
 						console.groupCollapsed(response.components.length + ' FIL components retrieved');
 						response.components.forEach(
-							component => console.log (component.language + ' ' + component.name));
+							component => console.log(component.language + ' ' + component.name));
 						console.groupEnd();
 					}
-			}),
-			switchMap ( (response: ComponentTree) => {
-				const languageCounts: ILanguageCount = {};
-				response.components.forEach(element => {
-					if (!languageCounts[element.language]) {
-						languageCounts[element.language] = 1;
-					} else {
-						languageCounts[element.language]++;
-					}
-				});
-				if (Constants.DEBUG) {
-					console.groupCollapsed ( key + ' files summary');
-					Object.entries(languageCounts).forEach( ([language, count]) => {
-						console.log(language, count);
+				}),
+				switchMap((response: ComponentTree) => {
+					const languageCounts: ILanguageCount = {};
+					response.components.forEach(element => {
+						if (!languageCounts[element.language]) {
+							languageCounts[element.language] = 1;
+						} else {
+							languageCounts[element.language]++;
+						}
 					});
-					console.groupEnd ();
-				}
-				return of(languageCounts);
-			}), catchError( (error)  => {
-				console.error ('error', error);
-				return of({});
-			})
-		);
+					if (Constants.DEBUG) {
+						console.groupCollapsed(key + ' files summary');
+						Object.entries(languageCounts).forEach(([language, count]) => {
+							console.log(language, count);
+						});
+						console.groupEnd();
+					}
+					return of(languageCounts);
+				}), catchError((error) => {
+					console.error('error', error);
+					return of({});
+				})
+			);
 	}
 
 	/**
@@ -308,7 +310,7 @@ export class SonarService extends InternalService {
 	}
 
 	evaluateSonarProject(project: Project, sonarKey: string): number {
-		const sonarProject = this.projectService.getSonarProject (project, sonarKey);
+		const sonarProject = this.projectService.getSonarProject(project, sonarKey);
 		let result = 0;
 		sonarProject.projectSonarMetricValues.forEach(metricValues => {
 			if (metricValues.weight) {
@@ -323,7 +325,7 @@ export class SonarService extends InternalService {
 						result += metricValues.weight * metricValues.value;
 						break;
 					case 'duplicated_lines_density':
-						result += metricValues.weight * ( 1 - metricValues.value / 100);
+						result += metricValues.weight * (1 - metricValues.value / 100);
 						break;
 					case 'sqale_rating':
 						if (metricValues.value < 60) {
