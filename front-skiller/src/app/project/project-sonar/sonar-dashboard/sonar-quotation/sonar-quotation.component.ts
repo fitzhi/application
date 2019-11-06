e38a564968ee98d265e6980566cc6dc84f958628
frvidal <frederic.vidal.perso@gmail.com>
2019-11-06 13:51:10 +0100

    [IMPL][Front] Displaying quotation badges for the metric values array
    This is the first implementation. Neither the real quotation is not displayed, nor the percentage.

--- front-skiller/src/app/project/project-sonar/sonar-dashboard/sonar-quotation/sonar-quotation.component.ts
+++ front-skiller/src/app/project/project-sonar/sonar-dashboard/sonar-quotation/sonar-quotation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, AfterViewInit, OnDestroy } from '@angular/core';
 import * as d3 from 'd3';
 import { Subject } from 'rxjs';
 import { PanelSwitchEvent } from '../../sonar-thumbnails/panel-switch-event';
@@ -7,13 +7,14 @@ import { SonarService } from 'src/app/service/sonar.service';
 import { Project } from 'src/app/data/project';
 import { ProjectService } from 'src/app/service/project.service';
 import { BadgeQuotation } from './badge-quotation';
+import { BaseComponent } from 'src/app/base/base.component';
 
 @Component({
 	selector: 'app-sonar-quotation',
 	templateUrl: './sonar-quotation.component.html',
 	styleUrls: ['./sonar-quotation.component.css']
 })
-export class SonarQuotationComponent implements OnInit {
+export class SonarQuotationComponent extends BaseComponent implements OnInit, OnDestroy {
 
 	/**
 	 * Observable throwing the current project.
@@ -44,20 +45,25 @@ export class SonarQuotationComponent implements OnInit {
 
 	constructor(
 		private sonarService: SonarService,
-		private projectService: ProjectService) { }
+		private projectService: ProjectService) { super(); }
 
 	ngOnInit() {
 
-		this.project$.subscribe(project => this.project = project);
+		this.subscriptions.add(
+			this.project$.subscribe(project => this.project = project));
 
-		this.panelSwitchTransmitter$.subscribe(
-			panelSwitchEvent => {
-				if (panelSwitchEvent.idPanel === Constants.PROJECT_SONAR_PANEL.SONAR) {
-					this.evaluations = [];
-					this.evaluateProject(panelSwitchEvent.keySonar);
+		this.subscriptions.add(
+			this.panelSwitchTransmitter$.subscribe(
+				panelSwitchEvent => {
+					if (panelSwitchEvent.idPanel === Constants.PROJECT_SONAR_PANEL.SONAR) {
+						this.evaluations = [];
+						// Project should have been already loaded for this component.
+						if (this.project) {
+							this.evaluateProject(panelSwitchEvent.keySonar);
+						}
+					}
 				}
-			}
-		);
+			));
 	}
 
 	/**
@@ -70,6 +76,25 @@ export class SonarQuotationComponent implements OnInit {
 			new BadgeQuotation(
 				'Global quotation',
 				this.sonarService.evaluateSonarProject(this.project, keySonar)));
+
+		const sonarProject = this.projectService.getSonarProject(this.project, keySonar);
+		if (!sonarProject) {
+			throw new Error ('Cannot retrieve the Sonar project ' + keySonar + ' for project ' + this.project.name);
+		}
+
+		sonarProject.projectSonarMetricValues.forEach( metricValue => {
+			this.evaluations.push(
+				new BadgeQuotation(
+					metricValue.key,
+					this.sonarService.evaluateSonarProject(this.project, keySonar)));
+			});
+	}
+
+	/**
+	 * Calling the base class to unsubscribe all subscriptions.
+	 */
+	ngOnDestroy() {
+		super.ngOnDestroy();
 	}
 
 }
