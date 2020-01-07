e4437ab1a867df7a4e2058a2c1dbc33fbef25090
frvidal <frederic.vidal.perso@gmail.com>
2020-01-07 15:12:28 +0100

    [IMPL][Front] Slice view behavior when clicked

--- front-skiller/src/app/app.module.ts
+++ front-skiller/src/app/app.module.ts
@@ -107,7 +107,7 @@ import { AuditAttachmentComponent } from './project/project-audit/project-audit-
 // tslint:disable-next-line:max-line-length
 import { AuditUploadAttachmentComponent } from './project/project-audit/project-audit-badges/files-detail-form/audit-attachment-upload/audit-upload-attachment/audit-upload-attachment.component';
 import { PieDashboardComponent } from './welcome/pie-dashboard/pie-dashboard.component';
-import { PieChartComponent } from './welcome/pie-chart/pie-chart.component';
+import { PieChartComponent } from './welcome/pie-dashboard/pie-chart/pie-chart.component';
 
 @NgModule({
 	declarations: [
