2ae2ee4f913765c0945d435d1f2a574ac65381be
frvidal <frederic.vidal.perso@gmail.com>
2020-02-19 11:29:16 +0100

    [Tweak][Script] Removing crossOrigin=use-credential param

--- front-fitzhi/deploy.sh
+++ front-fitzhi/deploy.sh
@@ -5,7 +5,7 @@ rm -rf assets
 rm -rf docs
 rm *
 cd -
-ng build --prod --crossOrigin=use-credentials --output-path ../../spoq/docs --base-href /
+ng build --prod --output-path ../../spoq/docs --base-href /
 cp -i ../../spoq/docs/index.html ../../spoq/docs/404.html
 mv ../../spoq/docs/* ../../spoq/
 echo spoq.io >../../spoq/CNAME
