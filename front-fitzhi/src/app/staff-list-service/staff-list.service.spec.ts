3ba7f49c19b64b113412704b5a3f83489dbde7a1
frvidal <frederic.vidal.perso@gmail.com>
2020-06-06 22:54:48 +0200

    [IMPL][Front]  Enhancing lookupSimilarStaff

--- front-fitzhi/src/app/staff-list-service/staff-list.service.spec.ts
+++ front-fitzhi/src/app/staff-list-service/staff-list.service.spec.ts
@@ -33,6 +33,18 @@ describe('ListStaffService', () => {
 
 	}));
 
+	it('We should find Jean-Paul 2 in the Staff team when a Jean Paul 2 has been saved',
+		inject([StaffListService], (service: StaffListService) => {
+
+		expect(service).toBeTruthy();
+
+		service.allStaff.push(createStaff(1, 'Jean Paul', '2', 'jp2', 'jp2'));
+
+		expect(service.lookupSimilarStaff(createStaff(1, 'Jean-Paul7', '2', 'jp2', 'jp2'))).toBeUndefined();
+
+	}));
+
+
 	function createStaff (idStaff: number, firstName: string, lastName: string, nickName: string, login: string) {
 		const collab = new Collaborator();
 		collab.idStaff = idStaff;
