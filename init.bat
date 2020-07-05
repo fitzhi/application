79b678e7b95de50b632f3434c93f5e0e2b98284e
frvidal <frederic.vidal.perso@gmail.com>
2020-07-05 11:43:53 +0200

    [FIX][Back] Test & Windows installation

--- init.bat
+++ init.bat
@@ -10,15 +10,15 @@ rmdir /Q /s application
 git clone https://github.com/fitzhi/application
 cd ..
 mkdir deploy\backend-fitzhi
+mkdir deploy\data
 cd back-fitzhi
-mvn package -DargLine="-Dfile_separator=\\ -Dfile.encoding=\"UTF-8\" -Dsun.jnu.encoding=\"UTF-8\""
-
+call mvn install -DargLine="-Dfile_separator=\\ -Dfile.encoding=\"UTF-8\" -Dsun.jnu.encoding=\"UTF-8\""
 echo ""
 echo "Building back-end Fitzhi"
 echo "------------------------"
-copy .\target\fitzhi.jar ..\deploy\backend-fitzhi\fitzhi.jar
-copy .\target\application.properties ..\deploy\backend-fitzhi\application.properties
-copy .\target\logback-spring.xml ..\deploy\backend-fitzhi\logback-spring.xml
-copy data ..\deploy\
+xcopy .\target\fitzhi.jar ..\deploy\backend-fitzhi\fitzhi.jar
+xcopy .\target\application.properties ..\deploy\backend-fitzhi\application.properties
+xcopy .\target\logback-spring.xml ..\deploy\backend-fitzhi\logback-spring.xml
+xcopy /E /I data ..\deploy\data
 cd ..
 
