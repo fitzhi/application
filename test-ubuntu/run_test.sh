9c67d75d05a3ce284ce0226731cb46cd3e181bae
frvidal <frederic.vidal.perso@gmail.com>
2020-07-14 21:40:38 +0200

    [IMPL][Back] Install and testing the application on Ubuntu

---
+++ test-ubuntu/run_test.sh
@@ -0,0 +1,2 @@
+docker pull ubuntu
+docker run -ti -v /Users/frvidal/work/projets/fitzhi/test-ubuntu:/script ubuntu /bin/bash script/test.sh
---
+++ test-ubuntu/test.sh
@@ -0,0 +1,8 @@
+apt update -y
+apt install openjdk-8-jdk -y
+apt-get install maven -y
+apt install git -y
+git clone https://github.com/fitzhi/application.git
+cd application
+./init.sh
+
