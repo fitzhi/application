5b411a835fab9016a665e335da3232107384ac6d
Frederic Vidal <frvidal@L540098.SQLI>
2020-08-14 21:46:54 +0200

    [FIX][Front][Back] Updating the importance of the dataChart

---
+++ back-fitzhi/src/test/java/com/fitzhi/source/crawler/git/GitCrawlerUpdateImportanceTest.java
@@ -0,0 +1,165 @@
+/**
+ * 
+ */
+package com.fitzhi.source.crawler.git;
+
+import static com.fitzhi.Global.DASHBOARD_GENERATION;
+import static com.fitzhi.Global.PROJECT;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fitzhi.bean.AsyncTask;
+import com.fitzhi.bean.DataChartHandler;
+import com.fitzhi.bean.DataHandler;
+import com.fitzhi.bean.ProjectDashboardCustomizer;
+import com.fitzhi.bean.ProjectHandler;
+import com.fitzhi.data.internal.DataChart;
+import com.fitzhi.data.internal.Ghost;
+import com.fitzhi.data.internal.Project;
+import com.fitzhi.data.internal.RepositoryAnalysis;
+import com.fitzhi.data.internal.RiskDashboard;
+import com.fitzhi.data.source.CommitRepository;
+import com.fitzhi.exception.SkillerException;
+import com.fitzhi.source.crawler.RepoScanner;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Fr&eacute;d&eacute;ric VIDAL
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class GitCrawlerUpdateImportanceTest {
+
+	private static final String TEST_PROJECT_ROCROI = "mock-repo-with-branches-for-dev-and-testing-purposes";
+
+	private static final String DIR_GIT = ".." + File.separator + "git_repo_for_test" + File.separator + "%s"
+			+ File.separator;
+
+	private static final String FILE_GIT = DIR_GIT + ".git";
+
+	@Autowired
+	@Qualifier("GIT")
+	RepoScanner scanner;
+
+	@Autowired
+	DataHandler dataSaver;
+
+	@Autowired
+	ProjectHandler projectHandler;
+
+	@Autowired
+	DataChartHandler dataChartHandler;
+
+	@Autowired
+	ProjectDashboardCustomizer projectDashboardCustomizer;
+
+	@Autowired
+	AsyncTask asyncTask;
+
+	private Repository repository;
+
+	private Project project;
+
+	private long lengthOfFile = 0;
+
+	@Before
+	public void before() throws SkillerException {
+		project = new Project(1643, TEST_PROJECT_ROCROI);
+		projectHandler.addNewProject(project);
+		project.setLocationRepository(String.format(DIR_GIT, TEST_PROJECT_ROCROI));
+		project.setGhosts(new ArrayList<Ghost>());
+	
+		File test = new File(project.getLocationRepository() + "/src/main/java/fr/test/main/Test.java");
+		Assert.assertEquals(true, test.exists());
+		lengthOfFile = test.length();
+
+		asyncTask.addTask(DASHBOARD_GENERATION, PROJECT, 1643);
+	}
+
+	/**
+	 * Test the method filterElibilible
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testUpdateImportance() throws SkillerException, IOException {
+		FileRepositoryBuilder builder = new FileRepositoryBuilder();
+		if (log.isDebugEnabled()) {
+			log.debug(String.format("Repository location %s", new File(String.format(FILE_GIT, TEST_PROJECT_ROCROI)).getAbsolutePath()));
+		}
+	
+		repository = builder.setGitDir(new File(String.format(FILE_GIT, TEST_PROJECT_ROCROI))).readEnvironment().findGitDir()
+				.build();
+        RepositoryAnalysis analysis = scanner.loadChanges(project, repository);		
+
+        scanner.updateImportance(project, analysis);
+
+        Assert.assertEquals(true, analysis.getChanges().getChanges().containsKey("src/main/java/fr/test/main/Test.java"));
+        SourceFileHistory sourceFileHistory = analysis.getChanges().getChanges().get("src/main/java/fr/test/main/Test.java"); 
+        Assert.assertEquals(lengthOfFile, sourceFileHistory.getImportance());;
+ 
+	}
+	
+	@Test
+	public void loadChangesForFirstTest() throws IOException, SkillerException {
+
+		FileRepositoryBuilder builder = new FileRepositoryBuilder();
+		repository = builder.setGitDir(new File(project.getLocationRepository() + "/.git")).readEnvironment().findGitDir()
+				.build();
+
+		CommitRepository commitRepository = scanner.parseRepository(project);
+		Assert.assertNotNull(commitRepository);
+
+		RiskDashboard data = scanner.aggregateDashboard(project, commitRepository);
+		Assert.assertNotNull(data);
+
+		DataChart dataChart = data.riskChartData.getChildren().get(0);
+		Assert.assertEquals("src", dataChart.getLocation());
+		
+		dataChart = dataChart.getChildren().get(0);
+		Assert.assertEquals("main", dataChart.getLocation());
+
+		dataChart = dataChart.getChildren().get(0);
+		Assert.assertEquals("java", dataChart.getLocation());
+
+		dataChart = dataChart.getChildren().get(0);
+		Assert.assertEquals("fr", dataChart.getLocation());
+
+		dataChart = dataChart.getChildren().get(0);
+		Assert.assertEquals("test", dataChart.getLocation());
+
+		
+		dataChartHandler.aggregateDataChart(data.riskChartData);
+		Assert.assertNotNull(data.riskChartData);
+		
+		dataChart = data.riskChartData.getChildren().get(0);
+		Assert.assertEquals("src/main/java/fr/test/main", dataChart.getLocation());
+		Assert.assertEquals(lengthOfFile, dataChart.getImportance());
+	}
+
+	@After
+	public void after() throws SkillerException {
+		if (repository != null) {
+			repository.close();
+		}
+		projectHandler.removeProject(1643);
+    	asyncTask.removeTask(DASHBOARD_GENERATION, PROJECT, 1643);
+ 	}
+}
---
+++ front-fitzhi/src/app/project/project-sunburst/project-sunburst-with-data.component.spec.ts
@@ -0,0 +1,68 @@
+
+import { async, ComponentFixture, TestBed, TestModuleMetadata } from '@angular/core/testing';
+import { ProjectSunburstComponent, PreviewContext } from './project-sunburst.component';
+import { InitTest } from 'src/app/test/init-test';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NodeDetailComponent } from './node-detail/node-detail.component';
+import { MatCardModule } from '@angular/material/card';
+import { ProjectGhostsComponent } from './project-ghosts/project-ghosts.component';
+import { TableDependenciesComponent } from './table-dependencies/table-dependencies.component';
+import { DialogLegendSunburstComponent } from './legend-sunburst/legend-sunburst.component';
+import { ListFilenamesComponent } from './node-detail/list-filenames/list-filenames.component';
+import { ListContributorsComponent } from './node-detail/list-contributors/list-contributors.component';
+import { TableGhostsComponent } from './project-ghosts/table-ghosts/table-ghosts.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SSEWatcherComponent } from './ssewatcher/ssewatcher.component';
+import { ProjectService } from 'src/app/service/project.service';
+import { Project } from 'src/app/data/project';
+import { MessageBoxComponent } from 'src/app/interaction/message-box/dialog/message-box.component';
+import { SunburstCinematicService } from './service/sunburst-cinematic.service';
+import { NgxPopper } from 'angular-popper';
+import { data } from './data-sunburst';
+import { SunburstCacheService } from './service/sunburst-cache.service';
+
+describe('ProjectSunburstComponent with data', () => {
+	let component: ProjectSunburstComponent;
+	let fixture: ComponentFixture<ProjectSunburstComponent>;
+
+	beforeEach(async(() => {
+		const testConf: TestModuleMetadata =  {
+			declarations: [ProjectSunburstComponent, NodeDetailComponent,
+				ProjectGhostsComponent, TableDependenciesComponent, DialogLegendSunburstComponent,
+				ListFilenamesComponent, ListContributorsComponent, TableGhostsComponent, SSEWatcherComponent, MessageBoxComponent],
+			providers: [],
+			imports: [MatSidenavModule, MatCardModule, NgxPopper, RouterTestingModule.withRoutes([])]
+		};
+		InitTest.addImports(testConf.imports);
+		InitTest.addProviders(testConf.providers);
+		TestBed.configureTestingModule(testConf).compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ProjectSunburstComponent);
+		component = fixture.componentInstance;
+		
+	});
+	
+	it('drawing the chart', () => {
+		
+		component.activeContext = PreviewContext.SUNBURST_READY;
+		fixture.detectChanges();
+
+		setTimeout(() => {			
+			const projectService = TestBed.inject(ProjectService);
+			projectService.project = new Project(1789, 'Revolutionnary project');
+			projectService.project.active = true;
+			projectService.projectLoaded$.next(true);
+			
+			const sunburstCacheService = TestBed.inject(SunburstCacheService);
+			sunburstCacheService.saveResponse(data);
+
+			const sunburstCinematicService = TestBed.inject(SunburstCinematicService);
+			sunburstCinematicService.refreshChart$.next(true);	
+		}, 0);
+
+		expect(component).toBeTruthy();
+
+	});
+});
