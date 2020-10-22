01ebaf2ea1baf5347380b324b35e66ae259ca293
Frederic Vidal <frvidal@L540098.SQLI>
2020-10-22 09:44:16 +0200

    Upgrading the test procedure

--- front-fitzhi/karma.conf.js
+++ front-fitzhi/karma.conf.js
@@ -25,14 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     singleRun: false,
-    restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
-      }
-    }  
+    restartOnFileChange: true  
   });
 };
