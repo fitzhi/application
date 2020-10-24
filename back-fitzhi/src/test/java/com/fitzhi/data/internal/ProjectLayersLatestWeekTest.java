5575c53e2285c46bd9314421d98a752ad2f8f35d
Frederic Vidal <frvidal@L540098.SQLI>
2020-10-24 16:09:38 +0200

    [IMPL][Back] JUnit test of ProjectLayers.latestWeek

---
+++ back-fitzhi/src/test/java/com/fitzhi/data/internal/ProjectLayersLatestWeekTest.java
@@ -0,0 +1,33 @@
+package com.fitzhi.data.internal;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Testing the method {@link ProjectLayers#LatestWeek()}
+ * 
+ * @author Fr&eacute;d&eacute;ric VIDAL
+ */
+public class ProjectLayersLatestWeekTest {
+    /**
+     * The logger for the SunburstDataTest class.
+     */
+    Logger logger = LoggerFactory.getLogger(SunburstDataTest.class.getCanonicalName());
+
+    /**
+     * Simple test of {@code latestWeek()}
+     */
+    @Test
+    public void testLatestWeek() {
+        ProjectLayers pl = new ProjectLayers(new Project(1, "test"));
+        pl.getLayers().add(new ProjectLayer(1, 2020, 3, 10, 1));
+        pl.getLayers().add(new ProjectLayer(1, 2020, 3, 20, 2));
+        pl.getLayers().add(new ProjectLayer(1, 2019, 23, 45, 1));
+        assertEquals(pl.LatestWeek().getYear(), 2020);
+        assertEquals(pl.LatestWeek().getWeek(), 3);
+        assertEquals(pl.LatestWeek().getIdProject(), 2);
+    }
+}
