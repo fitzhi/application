c5726b54d5d7b3101caa79f8e2f20670483d6be5
Frederic Vidal <frvidal@L540098.SQLI>
2020-11-30 22:14:48 +0100

    Updating the scan-docker.bat for the window based system

--- back-fitzhi/sonar/scan-docker.bat
+++ back-fitzhi/sonar/scan-docker.bat
@@ -1,2 +1,2 @@
 
-docker run --rm -e SONAR_HOST_URL="http://localhost:9000" --network=host -v "D:\work\projects\application\back-fitzhi/src/:/usr/src" sonarsource/sonar-scanner-cli -D sonar.login="admin" -D sonar.password="admin" -D sonar.projectKey="backend_Fitzhi" -Dsonar.java.binaries="../target/classes" -X
+docker run --rm -e SONAR_HOST_URL="http://sqli70253:9000" --network=host -v "D:\work\projects\application\back-fitzhi/src/:/usr/src" sonarsource/sonar-scanner-cli -D sonar.login="admin" -D sonar.password="admin" -D sonar.projectKey="backend_Fitzhi" -Dsonar.java.binaries="..\target\classes" -X
