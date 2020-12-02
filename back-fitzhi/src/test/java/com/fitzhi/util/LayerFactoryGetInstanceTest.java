a8c0423784aab701af3bffc26500738505b83f98
Frederic Vidal <frvidal@L540098.SQLI>
2020-12-02 23:30:20 +0100

    [IMPL][Back] Fixing the test

--- back-fitzhi/src/test/java/com/fitzhi/util/LayerFactoryGetInstanceTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/util/LayerFactoryGetInstanceTest.java
@@ -11,11 +11,14 @@ import com.fitzhi.source.crawler.git.SourceChange;
 import org.junit.Assert;
 import org.junit.Test;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Testing the method {@link LayerFactory#getInstance(com.fitzhi.source.crawler.git.SourceChange)}
  * 
  * @author Fr&eacute;d&eacute;ric VIDAL
  */
+@Slf4j
 public class LayerFactoryGetInstanceTest {
 
     Calendar calendar = Calendar.getInstance();
@@ -23,16 +26,20 @@ public class LayerFactoryGetInstanceTest {
     @Test
     public void testLastWeek() {
         calendar.set(Calendar.YEAR, 2019);
-        calendar.set(Calendar.MONTH, Calendar.DECEMBER);
-        
+        calendar.set(Calendar.MONTH, Calendar.DECEMBER);     
         calendar.set(Calendar.DAY_OF_MONTH, 29);
-        LocalDate date = calendar.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        ZoneId zoneId = ZoneId.of("Europe/Paris");
+        log.info(String.format("Current %s", zoneId.toString()));
+        log.info(String.format("System %s", ZoneId.systemDefault().toString()));
+        
+        LocalDate date = calendar.getTime().toInstant().atZone(zoneId).toLocalDate();
         Layer layer = LayerFactory.getInstance(new SourceChange(date, 1));
         Assert.assertEquals("Testing the year for date " + date, 2019, layer.getYear());
         Assert.assertEquals("Testing the week for date " + date, 52, layer.getWeek());
 
         calendar.set(Calendar.DAY_OF_MONTH, 30);
-        date = calendar.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        date = calendar.getTime().toInstant().atZone(zoneId).toLocalDate();
         layer = LayerFactory.getInstance(new SourceChange(date, 1));
         Assert.assertEquals("Testing the year for date " + date, 2020, layer.getYear());
         Assert.assertEquals("Testing the week for date " + date, 1, layer.getWeek());
