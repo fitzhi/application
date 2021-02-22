5bd28e478496eb3a969e8269397b3aa5a6e0ba30
fitzhi <frederic.vidal@fitzhi.com>
2021-02-22 08:10:44 +0100

    [Tweak][Back] Enhancement of the Sonar client launcher

--- sonar/start-docker-sonar.bat
+++ sonar/start-docker-sonar.bat
@@ -1,2 +1,3 @@
+docker stop fitzhi-sonarqube
 docker rm fitzhi-sonarqube
 docker run -d -v /opt/sonarqube/conf:/data --name fitzhi-sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 fitzhi-test:latest
