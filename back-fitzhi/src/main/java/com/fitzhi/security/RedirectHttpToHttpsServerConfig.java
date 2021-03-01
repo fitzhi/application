92620e1f7a92b9df65cfa1dee44a8e6dc15920dd
fitzhi <frederic.vidal@fitzhi.com>
2021-03-01 11:00:03 +0100

    [Back][IMPL] Redirection filter HTTP to HTTPS actually unplugged (a FIX is needed)

---
+++ back-fitzhi/src/main/java/com/fitzhi/security/RedirectHttpToHttpsServerConfig.java
@@ -0,0 +1,42 @@
+package com.fitzhi.security;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@ConditionalOnProperty(value="redirectHttpToHttps", havingValue="true")
+@Configuration
+public class RedirectHttpToHttpsServerConfig {
+
+	
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+		return tomcat;
+	}
+
+	private Connector getHttpConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+}
