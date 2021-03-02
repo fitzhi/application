9402b6f5e2b3d115ebe852310752a277570a3cc5
fitzhi <frederic.vidal@fitzhi.com>
2021-03-02 10:30:56 +0100

    [Front][IMPL] Front Fitzhi deployment

--- front-fitzhi/deploy.sh
+++ front-fitzhi/deploy.sh
@@ -1,13 +1,29 @@
 echo "Starting the deployment to spoq.io"
 echo "----------------------------------"
-cd ../../spoq/
+if [ ! -d "../../spoq/" ] 
+then
+    echo "Directory '../../spoq/' DOES NOT exist. We clone the SPOQ repository."
+    cd ../.. 
+    git clone https://github.com/fitzhi/spoq.git
+    cd spoq
+else
+    echo "Directory '../../spoq/' is found."
+    cd ../../spoq/
+fi
+echo "We clean the 'spoq' directory."
 rm -rf assets
 rm -rf docs
 rm *
-cd -
-ng build --prod --output-path ../../spoq/docs --base-href /
+cd ../application/front-fitzhi
+./gen-build-ts.sh
+ng build --prod --output-path ../../spoq/docs --base-href //
 cp -i ../../spoq/docs/index.html ../../spoq/docs/404.html
 mv ../../spoq/docs/* ../../spoq/
 echo spoq.io >../../spoq/CNAME
+echo "We PUSH the new application."
+cd ../../spoq
+git add -A && git commit -m 'new building Release'
+git push
+cd ../application/front-fitzhi
 echo "...Deployment is done"
 
--- front-fitzhi/gen-build-ts.sh
+++ front-fitzhi/gen-build-ts.sh
@@ -1,1 +1,3 @@
+echo "Generation of the Application build timestamp"
+echo "---------------------------------------------"
 echo "export const RunTimeFile = { buildtime: '$(date '+%Y-%m-%d %H:%M')' };" | tee src/environments/runtime-file.ts
