0af65d08d7ef8018c19e3d17d655b6480245fb8a
fitzhi <frederic.vidal@fitzhi.com>
2021-03-02 19:35:35 +0100

    [IMPL][Front]  Unplugging the delivery

--- front-fitzhi/deploy.sh
+++ front-fitzhi/deploy.sh
@@ -27,7 +27,6 @@ git config user.email "frederic.vidal@fitzhi.com"
 echo "setting user.name to frvidal"
 git config user.name "frvidal"
 git add -A && git commit -m 'new building Release'
-echo {{$GITHUB_ACTOR}}
 git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/fitzhi/spoq.git"
 cd ../application/front-fitzhi
 echo "...Deployment is done"
