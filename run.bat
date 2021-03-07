6eca2ea6bf813f7fd936c82be8ccbcc0939bdd5e
fitzhi <frederic.vidal@fitzhi.com>
2021-03-07 22:28:39 +0100

    [IMPL][Back] Dual start HTTP vs HTTPS of Fitzhi

--- run.bat
+++ run.bat
@@ -1,6 +1,20 @@
-echo "Starting backend Fitzhi"
-echo "-----------------------"
-echo "" 
+echo off
+echo(
+if /i "%1" == "https" (
+    echo Starting backend Fitzhi in HTTPS mode
+    copy .\back-fitzhi\src\main\resources\application-https.properties .\deploy\backend-fitzhi\application-https.properties
+)
+if /i "%1" == "http" (
+    echo Starting backend Fitzhi in HTTP mode
+    del .\deploy\backend-fitzhi\application-https.properties
+)
+if NOT "%1" == "http" (
+    if NOT "%1" == "https" (
+        echo Invalid profile given. Expecting either HTTP, pr HTTPS
+    )
+)
+echo --------------------------------------
+echo(
 cd ./deploy/backend-fitzhi/
-java -jar fitzhi.jar
+java -jar fitzhi.jar --spring.profiles.active=%1
 
