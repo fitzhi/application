2934ea6eb411867e8411ab9ff4dce7a9fa7119ac
fitzhi <frederic.vidal@fitzhi.com>
2021-03-12 17:07:00 +0100

    [Front][Tweak] Polish of HttpErrorInterceptor logging

--- front-fitzhi/src/app/admin/service/http/http-error-interceptor-service.ts
+++ front-fitzhi/src/app/admin/service/http/http-error-interceptor-service.ts
@@ -1,5 +1,5 @@
 import { retry, catchError, tap } from 'rxjs/operators';
-import { HttpHandler, HttpEvent, HttpErrorResponse, HttpRequest, HttpInterceptor } from '@angular/common/http';
+import { HttpHandler, HttpEvent, HttpErrorResponse, HttpRequest, HttpInterceptor, HttpResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Injectable, Injector } from '@angular/core';
 import { MessageService } from '../../../interaction/message/message.service';
@@ -21,9 +21,18 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
 				retry(0),
 				tap(response => {
 					if (traceOn()) {
-						console.groupCollapsed ('%s %s', request.method, request.url);
-						console.log (response);
-						console.groupEnd();
+						if (response instanceof HttpResponse) {
+							// We do not log message if the HTTP request is sucessful
+							if (response.status !== HttpCodes.success) {
+								console.groupCollapsed ('%s %s', request.method, request.url);
+								console.log (response);
+								console.groupEnd();
+							}
+						} else {
+							console.groupCollapsed ('WTF : Exotic response %s %s', request.method, request.url);
+							console.log (response);
+							console.groupEnd();
+						}
 					}
 				}),
 				catchError((response: HttpErrorResponse) => {
