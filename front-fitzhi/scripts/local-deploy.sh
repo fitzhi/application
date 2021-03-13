d7a41fc9bf9c28a5a13044d40619c275f86f71c3
fitzhi <frederic.vidal@fitzhi.com>
2021-03-13 09:57:54 +0100

    [Front] script polish

--- front-fitzhi/scripts/local-deploy.sh
+++ front-fitzhi/scripts/local-deploy.sh
@@ -1,5 +1,5 @@
-echo "Building the Fitzhi Angular application and local copy"
-echo "------------------------------------------------------"
+echo "Building the Fitzhi Angular application and local copy."
+echo "-------------------------------------------------------"
 rm  -rf  /var/www/fitzhi
 mkdir /var/www/fitzhi
 npm run prod-var-www-fithi
