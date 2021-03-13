69ec17b3cb4dcb6ed74ae1b86dbc5c2a8149fa86
fitzhi <frederic.vidal@fitzhi.com>
2021-03-13 14:09:51 +0100

    [Front[Fix] 1 missing char in a script

--- front-fitzhi/scripts/local-deploy.sh
+++ front-fitzhi/scripts/local-deploy.sh
@@ -3,5 +3,8 @@ echo "-------------------------------------------------------"
 rm  -rf  /var/www/fitzhi
 mkdir /var/www/fitzhi
 cd ..
+apt-get update
+apt install npm
+npm i
 sh ./gen-build-ts.sh
-npm run prod-var-www-fithi
+npm run prod-var-www-fitzhi
