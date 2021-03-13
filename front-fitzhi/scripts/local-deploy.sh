4ded735f062acd051bf44bde2244ec7cb2cf2a6c
fitzhi <frederic.vidal@fitzhi.com>
2021-03-13 10:02:53 +0100

    [Front] Fix installation script

--- front-fitzhi/scripts/local-deploy.sh
+++ front-fitzhi/scripts/local-deploy.sh
@@ -2,4 +2,6 @@ echo "Building the Fitzhi Angular application and local copy."
 echo "-------------------------------------------------------"
 rm  -rf  /var/www/fitzhi
 mkdir /var/www/fitzhi
+cd ..
+sh ./gen-build-ts.sh
 npm run prod-var-www-fithi
