cc7c3e23840a7a9820a23b0274646471171fbe02
fitzhi <frederic.vidal@fitzhi.com>
2021-03-15 21:22:00 +0100

    [Back] Update an installation script for Ubuntu

--- test-ubuntu/test.sh
+++ test-ubuntu/test.sh
@@ -1,8 +1,10 @@
 apt update -y
+apt-get install language-pack-fr
+update-locale LANG=fr_FR.utf8
 apt install openjdk-8-jdk -y
 apt-get install maven -y
 apt install git -y
-git clone https://github.com/fitzhi/application.git
+git clone --branch release-1.4 https://github.com/fitzhi/application.git
 cd application
 ./init.sh
 
