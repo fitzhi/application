6d04a5ab0846f6b5553875c126c8351be5e4f1ee
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-05-07 17:58:50 +0200

    [Back] Polish

--- back-fitzhi/src/main/java/com/fitzhi/controller/ProjectGhostController.java
+++ back-fitzhi/src/main/java/com/fitzhi/controller/ProjectGhostController.java
@@ -8,7 +8,6 @@ import com.fitzhi.data.internal.Project;
 import com.fitzhi.exception.ApplicationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
