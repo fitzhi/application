a9a718b3767d11766b3fded02b8f6c3712a5d369
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-05-16 20:31:41 +0200

    [Front][Fix] Fixing a test

--- front-fitzhi/src/app/tabs-project/project-audit/project-audit-badges/report-detail-form/report-detail-form.component.spec.ts
+++ front-fitzhi/src/app/tabs-project/project-audit/project-audit-badges/report-detail-form/report-detail-form.component.spec.ts
@@ -48,7 +48,7 @@ describe('ReportDetailFormComponent', () => {
 		component = fixture.componentInstance;
 		projectService = TestBed.inject(ProjectService);
 		projectService.project = new Project(1, 'Testing project');
-		projectService.project.audit['1'] = new AuditTopic(1, 100, 100, 'This is a perfect topic', []);
+		projectService.project.audit['1'] = new AuditTopic(1, 1, 100, 100, 'This is a perfect topic', []);
 		projectService.projectLoaded$.next(true);
 
 		referentialService = TestBed.inject(ReferentialService);
