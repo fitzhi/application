d831a245256ae1e0b15a3c80bcaae7c3705c8a4e
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-07-18 21:58:21 +0200

    [Front][FIX] Startup process for the first connection

--- front-fitzhi/src/app/admin/starting-setup/starting-setup.component.ts
+++ front-fitzhi/src/app/admin/starting-setup/starting-setup.component.ts
@@ -19,34 +19,45 @@ import { Subject } from 'rxjs';
 export class StartingSetupComponent extends BaseComponent implements OnDestroy {
 
 	/**
-     * The main stepper is passed in order to procede a programmatly step.next().
-     */
+	 * The main stepper is passed in order to procede a programmatly step.next().
+	 */
 	@ViewChild('stepper', { static: true }) stepper: MatStepper;
 
 	/**
-     * Are we in the very first connection ?
-     */
-	public veryFirstConnection = new Subject<boolean>();
+	 * This status will be setup to TRUE, FALSE otherwise.
+	 */
+	 public veryFirstConnection = true;
+
+	 /**
+	 * Are we in the very first connection ?
+	 */
+	private _veryFirstConnection$ = new Subject<boolean>();
+
+	 /**
+	 * Are we in the very first connection ?
+	 */
+	public veryFirstConnection$ = this._veryFirstConnection$.asObservable();
 
 	/**
-     * Array representing the fact that each step has been completed.
-     */
+	 * Array representing the fact that each step has been completed.
+	 */
 	completed: Array<boolean> = [false, false, false, false];
 
 	/**
-     * Current staff identifier.
-     */
+	 * Current staff identifier.
+	 */
 	idStaff: number;
 
 	/**
-     * Current staff.
-     */
+	 * Current staff.
+	 */
 	staff: Collaborator;
 
 	/**
 	 * Label of the second step.
 	 */
 	labelUser = 'User';
+
 
 	constructor(
 		private backendSetupService: BackendSetupService,
@@ -57,14 +68,15 @@ export class StartingSetupComponent extends BaseComponent implements OnDestroy {
 		private httpClient: HttpClient) { super(); }
 
 	/**
-     * Setup the fact that this is the very first connection.
-     */
+	 * Setup the fact that this is the very first connection.
+	 */
 	onChangeVeryFirstConnection($event: boolean) {
 		if (traceOn()) {
-			console.log('veryFirstConnecion :', $event);
+			console.log('veryFirstConnection :', $event);
 		}
 
-		this.veryFirstConnection.next($event);
+		this.veryFirstConnection = $event;
+		this._veryFirstConnection$.next(this.veryFirstConnection);
 
 		this.labelUser = ($event) ? 'First admin user' : 'First registration';
 
@@ -72,14 +84,12 @@ export class StartingSetupComponent extends BaseComponent implements OnDestroy {
 		this.referentialService.loadAllReferentials();
 		this.skillService.loadSkills();
 
-		setTimeout(() => {
-			this.stepper.next();
-		}, 0);
+		setTimeout(() => this.stepper.next(), 0);
 	}
 
 	/**
-     * Catch the staff identifier created the registerUserComponent.
-     */
+	 * Catch the staff identifier created the registerUserComponent.
+	 */
 	setRegisteredUser($event: number) {
 
 		if (traceOn()) {
@@ -95,9 +105,7 @@ export class StartingSetupComponent extends BaseComponent implements OnDestroy {
 		}
 		this.completed[1] = true;
 		this.idStaff = $event;
-		setTimeout(() => {
-			this.stepper.next();
-		}, 0);
+		setTimeout(() => this.stepper.next(), 0);
 	}
 
 	/**
@@ -113,8 +121,8 @@ export class StartingSetupComponent extends BaseComponent implements OnDestroy {
 	}
 
 	/**
-     * Catch the fact that the user is successfully connected, OR NOT...
-     */
+	 * Catch the fact that the user is successfully connected, OR NOT...
+	 */
 	setConnection($event: boolean) {
 		if ($event) {
 			this.completed[2] = true;
@@ -131,44 +139,52 @@ export class StartingSetupComponent extends BaseComponent implements OnDestroy {
 	}
 
 	/**
-     * Catch the complete staff entity, updated by the staff-form.
-     */
+	 * Catch the complete staff entity, updated by the staff-form.
+	 * @param $event the staff member created
+	 */
 	setStaffUpdatedForUser($event: Collaborator) {
 		if (traceOn()) {
 			console.log('staff updated for :', $event.lastName);
 		}
 
-		this.subscriptions.add(
-			this.httpClient.post<Boolean>(this.backendSetupService.url() + '/admin/saveVeryFirstConnection', '')
-				.subscribe({
-					next:  veryFirstConnectionIsRegistered => {
-						if (traceOn() && veryFirstConnectionIsRegistered) {
-							console.log('The very first connection is registered into Fitzhi.');
+		if (!this.veryFirstConnection) {
+			if (traceOn()) {
+				console.log('We are not in mode "very first connection".');
+			}
+			this.nextStepAfterStaffUpdate($event);
+		} else {
+			this.subscriptions.add(
+				this.httpClient.post<Boolean>(this.backendSetupService.url() + '/admin/saveVeryFirstConnection', '')
+					.subscribe({
+						next:  veryFirstConnectionIsRegistered => {
+							if (traceOn() && veryFirstConnectionIsRegistered) {
+								console.log('The very first connection is registered into Fitzhi.');
+							}
+							this.nextStepAfterStaffUpdate($event);
 						}
-						this.completed[3] = true;
-						this.installService.installComplete();
-
-						this.staff = $event;
-						setTimeout(() => {
-							this.stepper.next();
-						}, 0);
 					}
-				}
-			)
-		);
+				)
+			);
+		}
 	}
 
+	private nextStepAfterStaffUpdate(staff: Collaborator): void {
+		this.completed[3] = true;
+		this.installService.installComplete();
+		this.staff = staff;
+		setTimeout(() => this.stepper.next(), 0);
+	}
 
 	/**
-     * @param $event we move from one step in the installation.
-     */
+	 * @param $event we move from one step in the installation.
+	 */
 	selectionChange($event) {
 		window.scroll(0, 0);
 	}
 
 	/**
-     * Calling the base class to unsubscribe all subscriptions.
-     */
+	 * Calling the base class to unsubscribe all subscriptions.
+	 */
 	ngOnDestroy() {
 		super.ngOnDestroy();
 	}
