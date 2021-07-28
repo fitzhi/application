7d76af7143798fc84ad370ff9b245ac814ff353f
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-07-28 08:10:23 +0200

    [Front] Polish

--- front-fitzhi/src/app/service/password-confirmation-must-match-validator.ts
+++ front-fitzhi/src/app/service/password-confirmation-must-match-validator.ts
@@ -10,21 +10,21 @@ import { FormGroup, ValidatorFn } from '@angular/forms';
 export class PasswordConfirmationMustMatchValidator {
 	public check(): ValidatorFn {
 		return (formGroup: FormGroup) => {
-			
+
 			const control = formGroup.controls['password'];
 			const matchingControl = formGroup.controls['passwordConfirmation'];
-	
+
 			// return if another validator has already found an error on the matchingControl
 			if (matchingControl.errors) {
 				return null;
 			}
-	
+
 			// set error on matchingControl if validation fails
 			if (control.value !== matchingControl.value) {
 				return ({ mustMatch: true });
 			} else {
 				return null;
 			}
-		}
+		};
 	}
-} 
+}
