9041ed7081b357df06a246be4c7f119d1fe7a6f7
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-08-25 07:11:25 +0200

    [Back][IMPL] Display periodicaly the active tasks

--- back-fitzhi/src/main/java/com/fitzhi/scheduling/ScheduledTasks.java
+++ back-fitzhi/src/main/java/com/fitzhi/scheduling/ScheduledTasks.java
@@ -1,5 +1,9 @@
 package com.fitzhi.scheduling;
 
+import java.util.Calendar;
+
+import com.fitzhi.Global;
+import com.fitzhi.bean.AsyncTask;
 import com.fitzhi.bean.ProjectHandler;
 import com.fitzhi.source.crawler.BatchRepositoryCrawler;
 
@@ -20,17 +24,29 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class ScheduledTasks {
 
-	@Autowired
-	BatchRepositoryCrawler batchRepoScanner;
-
 	@Value("${cron.code.analysis}")
 	private String cronCodeAnalysis;
 	
 	@Value("${cron.experiences.detection}")
 	private String cronExperiencesDetection;
 
+	@Value("${cron.tasks.report}")
+	private String cronTasksReport;
+
+	@Autowired
+	BatchRepositoryCrawler batchRepoScanner;
+
 	@Autowired
 	ProjectHandler projectHandler;
+
+	@Autowired
+	AsyncTask asyncTask;
+
+	/**
+	 * Current minute of the execution of the medthiod {@link #tasksReport}.
+	 * This property is used in a work-around 
+	 */
+	int minute = -1;
 
 	@Scheduled(cron = "${cron.code.analysis}")
 	public void codeAnalysis() {
@@ -54,6 +70,21 @@ public class ScheduledTasks {
 			projectHandler.updateStaffSkillLevel(projectHandler.processGlobalExperiences());
 			if (log.isInfoEnabled()) {
 				log.info("Peacefully terminate the experiences detection.");
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	@Scheduled(cron = "${cron.tasks.report}")
+	public void tasksReport() {
+		try {
+			// For an unknown, this method is executed multiple times when the cron is equal to '* 0/5 * * * ?'
+			if (minute != Calendar.getInstance().get(Calendar.MINUTE)) {
+				minute = Calendar.getInstance().get(Calendar.MINUTE);
+				if (log.isInfoEnabled()) {
+					log.info(Global.LN + "Current active tasks :" + Global.LN + asyncTask.trace() + Global.LN);
+				}
 			}
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
