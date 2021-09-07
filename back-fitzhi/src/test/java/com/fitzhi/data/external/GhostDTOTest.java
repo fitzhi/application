4157f933b6c494e8fd67d129eb0078a60376d98a
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2021-09-07 22:28:11 +0200

    [Back][Fix] test GhostDTO

--- back-fitzhi/src/test/java/com/fitzhi/data/external/GhostDTOTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/data/external/GhostDTOTest.java
@@ -10,6 +10,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+
 import com.fitzhi.data.internal.Committer;
 import com.google.gson.Gson;
 
@@ -17,9 +21,8 @@ import com.google.gson.Gson;
  * @author Fr&eacute;d&eacute;ric VIDAL
  *
  */
+@Slf4j
 public class GhostDTOTest {
-
-	Logger logger = LoggerFactory.getLogger(GhostDTOTest.class.getCanonicalName());
 
 	@Test
 	public void testSimple() {
@@ -28,12 +31,25 @@ public class GhostDTOTest {
 		pseudo.setPseudo("test");
 		pseudo.setLogin("loginTest");
 		pseudo.setAction(Action.A);
+		pseudo.setFirstCommit(LocalDate.of(2021, 1, 1));
+		pseudo.setLastCommit(LocalDate.of(2021, 1, 2));
+		pseudo.setNumberOfCommits(1);		
+		pseudo.setNumberOfFiles(2);		
 		Gson g = new Gson();
 
-		Assert.assertTrue("{\"pseudo\":\"test\",\"idStaff\":1,\"login\":\"loginTest\",\"technical\":false,\"action\":\"A\"}".equals(g.toJson(pseudo)));
-		if (logger.isDebugEnabled()) {
-			logger.debug(g.toJson(pseudo)); 
+		String res = g.toJson(pseudo);
+		if (log.isDebugEnabled()) {
+			log.debug(res); 
 		}
+
+		Committer committer = g.fromJson(res, Committer.class);
+		Assert.assertEquals(1, committer.getIdStaff());
+		Assert.assertEquals(LocalDate.of(2021, 1, 1), committer.getFirstCommit());
+		Assert.assertEquals(LocalDate.of(2021, 1, 2), committer.getLastCommit());
+		Assert.assertEquals(1, committer.getNumberOfCommits());
+		Assert.assertEquals(2, committer.getNumberOfFiles());
+		Assert.assertEquals("loginTest", committer.getLogin());
+		Assert.assertEquals("test", committer.getPseudo());
 	}
 
 }
