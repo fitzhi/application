70aed2f6fc4b5dfca3d394211c0545cfe719e684
frvidal <frederic.vidal@fitzhi.com>
2021-10-17 23:27:55 +0200

    [Front][Back] [Fix] Fitzhi launcher in Docker

--- docker/docker/start-fitzhi.sh
+++ docker/docker/start-fitzhi.sh
@@ -1,8 +1,8 @@
-service nginx start 
 if [ $urlSonarServer ] && [ ${urlSonarServer} != "" ]
 then
     echo ${urlSonarServer}
     sed -i 's#proxy_pass http://host.docker.internal:9000/sonar;#proxy_pass '${urlSonarServer}';#g' /etc/nginx/sites-enabled/default
     cat ../data/referential/sonar-servers.json
 fi
+service nginx start 
 java -Xmx1g -jar fitzhi.jar --spring.profiles.active=HTTP
