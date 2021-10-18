8bc871bde09ec72744db3dc01960ab843331dc47
frvidal <frederic.vidal@fitzhi.com>
2021-10-18 22:36:23 +0200

    [Docker] [Rework] start-fitzhi.sh

--- docker/docker/start-fitzhi.sh
+++ docker/docker/start-fitzhi.sh
@@ -1,21 +1,22 @@
-if [ $urlSonarServer ] && [ $urlSonarServer != "" ]
+if [ ${urlSonarServer} ] && [ ${urlSonarServer} != "" ]
 then
-	echo ${urlSonarServer}
+	echo Sonar URL ${urlSonarServer}
 	sed -i 's#proxy_pass http://host.docker.internal:9000/sonar;#proxy_pass '$urlSonarServer';#g' /etc/nginx/sites-enabled/default
 	cat ../data/referential/sonar-servers.json
 fi
 
-if [ $user ] && [ $user != "" ]
+if [ ${user} ] && [ ${user} != "" ]
 then
-	echo $user
-	sed -i 's/"user": "admin",/"user": "'$user'",/g' ../data/referential/sonar-servers.json
+	echo Given user ${user}
+	sed -i 's/"user": "admin",/"user": "'${user}'",/g' ../data/referential/sonar-servers.json
 fi
 
-if [ $user ] && [ $user != "" ]
+if [ ${password} ] && [ ${password} != "" ]
 then
-	echo $user
-	sed -i 's/"password": "#password"/"password": "'$password'"/g' ../data/referential/sonar-servers.json
+	echo Given password ${password}
+	sed -i 's/"password": "#password"/"password": "'${password}'"/g' ../data/referential/sonar-servers.json
 fi
 
 service nginx start 
+
 java -Xmx1g -jar fitzhi.jar --spring.profiles.active=HTTP
