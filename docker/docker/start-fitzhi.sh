e519459eeeb8b7c8fb795e768401040b135bc896
frvidal <frederic.vidal@fitzhi.com>
2021-10-18 19:58:53 +0200

    [Front][Back] Docker Sonar connection settings from the Docker container

--- docker/docker/start-fitzhi.sh
+++ docker/docker/start-fitzhi.sh
@@ -1,8 +1,21 @@
-if [ $urlSonarServer ] && [ ${urlSonarServer} != "" ]
+if [ $urlSonarServer ] && [ $urlSonarServer != "" ]
 then
-    echo ${urlSonarServer}
-    sed -i 's#proxy_pass http://host.docker.internal:9000/sonar;#proxy_pass '${urlSonarServer}';#g' /etc/nginx/sites-enabled/default
-    cat ../data/referential/sonar-servers.json
+	echo ${urlSonarServer}
+	sed -i 's#proxy_pass http://host.docker.internal:9000/sonar;#proxy_pass '$urlSonarServer';#g' /etc/nginx/sites-enabled/default
+	cat ../data/referential/sonar-servers.json
 fi
+
+if [ $user ] && [ $user != "" ]
+then
+	echo $user
+	sed -i 's/"user": "admin",/"user": "'$user'",/g' ../data/referential/sonar-servers.json
+fi
+
+if [ $user ] && [ $user != "" ]
+then
+	echo $user
+	sed -i 's/"password": "#password"/"password": "'$password'"/g' ../data/referential/sonar-servers.json
+fi
+
 service nginx start 
 java -Xmx1g -jar fitzhi.jar --spring.profiles.active=HTTP
