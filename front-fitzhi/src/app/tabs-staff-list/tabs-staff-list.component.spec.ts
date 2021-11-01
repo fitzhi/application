08caf2125cc6da691456fc74d1272d9e94f2e5f5
frvidal <frederic.vidal@fitzhi.com>
2021-11-01 12:00:54 +0100

    [Front] Polish

--- front-fitzhi/src/app/tabs-staff-list/tabs-staff-list.component.spec.ts
+++ front-fitzhi/src/app/tabs-staff-list/tabs-staff-list.component.spec.ts
@@ -1,12 +1,12 @@
 import { ComponentFixture, TestBed, TestModuleMetadata, waitForAsync } from '@angular/core/testing';
-import { TabsStaffListComponent } from './tabs-staff-list.component';
-import { InitTest } from '../test/init-test';
-import { StaffListComponent } from './staff-list/staff-list.component';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ListCriteria } from '../data/listCriteria';
 import { StaffListContext } from '../data/staff-list-context';
+import { InitTest } from '../test/init-test';
 import { TabsStaffListService } from './service/tabs-staff-list.service';
-import { By } from '@angular/platform-browser';
+import { StaffListComponent } from './staff-list/staff-list.component';
+import { TabsStaffListComponent } from './tabs-staff-list.component';
 
 describe('TabsStaffListComponent', () => {
 	let component: TabsStaffListComponent;
@@ -27,12 +27,13 @@ describe('TabsStaffListComponent', () => {
 		fixture.detectChanges();
 	}));
 
-	it('Only the help container should be visible when any tab is created', () => {
+	it('Only the help container should be visible when any tab is created', done => {
 		expect(component).toBeTruthy();
 		expect(fixture.debugElement.query(By.css('#help-search'))).toBeDefined();
+		done();
 	});
 
-	it('The creation of a tab hides the help container', () => {
+	it('The creation of a tab hides the help container', done => {
 		component.tabKeys.push('title');
 		component.tabs.push('title');
 		const tabsStaffListService = TestBed.inject(TabsStaffListService);
@@ -40,5 +41,6 @@ describe('TabsStaffListComponent', () => {
 		fixture.detectChanges();
 		console.log (fixture.debugElement.query(By.css('#help-search')));
 		expect(fixture.debugElement.query(By.css('#help-search'))).toBeNull();
+		done();
 	});
 });
