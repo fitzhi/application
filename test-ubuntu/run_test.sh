05625f33c22fcac459cc6f3a9863e9c942df35fc
frvidal <frederic.vidal@fitzhi.com>
2021-12-14 20:26:49 +0100

    [Back][Tweak] Complete the test script for Ubuntu

--- test-ubuntu/run_test.sh
+++ test-ubuntu/run_test.sh
@@ -1,3 +1,3 @@
 docker pull ubuntu
-# docker run -ti -v /Users/fredericvidal/work/projects/application/test-ubuntu:/script ubuntu /bin/bash script/test.sh
-docker run -ti -v /Users/fredericvidal/work/projects/application/test-ubuntu:/script ubuntu /bin/bash
+# docker run -ti -v /Users/fredericvidal/work/projects/application/test-ubuntu:/script ubuntu /bin/bash
+docker run -ti -v /Users/fredericvidal/work/projects/application/test-ubuntu:/script ubuntu /bin/bash script/test.sh
