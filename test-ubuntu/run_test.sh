3cd79e73165ed787e215aef33c2b30a77b797a65
frvidal <frederic.vidal@fitzhi.com>
2021-12-14 20:25:29 +0100

    [Back][Tweak] Update the tests script for Ubuntu

--- test-ubuntu/run_test.sh
+++ test-ubuntu/run_test.sh
@@ -1,2 +1,3 @@
 docker pull ubuntu
-docker run -ti -v /Users/frvidal/work/projets/fitzhi/test-ubuntu:/script ubuntu /bin/bash script/test.sh
+# docker run -ti -v /Users/fredericvidal/work/projects/application/test-ubuntu:/script ubuntu /bin/bash script/test.sh
+docker run -ti -v /Users/fredericvidal/work/projects/application/test-ubuntu:/script ubuntu /bin/bash
--- test-ubuntu/test.sh
+++ test-ubuntu/test.sh
@@ -1,10 +1,10 @@
 apt update -y
 apt-get install language-pack-fr
 update-locale LANG=fr_FR.utf8
-apt install openjdk-8-jdk -y
+apt install openjdk-11-jre -y
 apt-get install maven -y
 apt install git -y
-git clone --branch release-1.4 https://github.com/fitzhi/application.git
+git clone --branch release-1.7 https://github.com/fitzhi/application.git
 cd application
 ./init.sh
 
