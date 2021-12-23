c1eebcfe6393a452e8a83f0f4ff015d173866c46
frvidal <frederic.vidal@fitzhi.com>
2021-12-23 17:22:32 +0100

    Revert "Revert "[Back][Rework] Encryption algorythm set to "DES/CBC/PKCS5Padding"""
    This reverts commit aa630c0ffbc4d6cbdb038766a829c268848f038e.

--- back-fitzhi/src/main/java/com/fitzhi/data/encryption/DataEncryption.java
+++ back-fitzhi/src/main/java/com/fitzhi/data/encryption/DataEncryption.java
@@ -3,13 +3,14 @@ package com.fitzhi.data.encryption;
 import static com.fitzhi.Error.CODE_ENCRYPTION_FAILED;
 import static com.fitzhi.Error.MESSAGE_ENCRYPTION_FAILED;
 
+import java.security.Key;
 import java.text.MessageFormat;
-import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 
 import com.fitzhi.exception.ApplicationException;
 
@@ -23,22 +24,37 @@ import com.fitzhi.exception.ApplicationException;
  */
 public class DataEncryption {
 
-	private static byte[] decodedKey = Base64.getDecoder().decode("techxhi128bitkey");
+	// DES Key
+	private static byte[] dataEncryptionStandartKey = "Fitzhi_128bitkey".getBytes();
 
-	private static SecretKey originalKey = null;
+	// IV Key
+	private static byte[] initializationVectorKey = "vector_1789".getBytes();
 	
+	private static IvParameterSpec ivSpec;
+
+	private static Key secretKey = null;
+
 	/**
 	 * Security transformation
 	 */
-	private final static String TRANSFORMATION = "AES"; // Good one for Sonar : "AES/GCM/NoPadding";
+	private final static String TRANSFORMATION = "DES/CBC/PKCS5Padding"; // Good one for Sonar : "AES/GCM/NoPadding";
 
-	// TODO AES is not a safe transformation. A keystore has to be installed. See UnpluggedDataEncryption.
 	private static Cipher getCipher() throws Exception {
-		return Cipher.getInstance(TRANSFORMATION); //NOSONAR
+		return Cipher.getInstance(TRANSFORMATION); 
+	}
+
+	private static void initializeSecrets() throws Exception {
+		// rebuild key using SecretKeySpec
+		if (secretKey == null) {
+			DESKeySpec dks = new DESKeySpec(dataEncryptionStandartKey);
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+			secretKey = keyFactory.generateSecret(dks);
+			ivSpec = new IvParameterSpec(initializationVectorKey);
+		}		
 	}
 
 	/**
-	 * <b>ENCRYPT</b> a message
+	 * <b>ENCRYPT</b> a message using the DES encryption mecanism.
 	 * @param data the data to encrypt (actually password only are encrypted)
 	 * @return the data encrypted 
 	 * @throws ApplicationException thrown if the encryption fails
@@ -48,45 +64,53 @@ public class DataEncryption {
 		try {
 
 			Cipher cipher = getCipher();
-            
-            // rebuild key using SecretKeySpec
-            if (originalKey == null) {
-            	originalKey = new SecretKeySpec(Arrays.copyOf(decodedKey, 16), TRANSFORMATION);
+			
+			initializeSecrets();
+			
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
+		
+			int blockSize = cipher.getBlockSize();
+			byte[] dataBytes = data.getBytes();
+            int plaintextLength = dataBytes.length;
+            if (plaintextLength % blockSize != 0) {
+                plaintextLength = plaintextLength + (blockSize - (plaintextLength % blockSize));
             }
 
-			cipher.init(Cipher.ENCRYPT_MODE, originalKey);
-			byte[] cipherText = cipher.doFinal(data.getBytes());
-			
-            return Base64.getEncoder().encodeToString(cipherText);
+            byte[] plaintext = new byte[plaintextLength];
+            System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
+						
+			byte[] cipherText = cipher.doFinal(plaintext);
+
+			return Base64.getEncoder().encodeToString(cipherText);
+
 		} catch (final Exception e) {
+			e.printStackTrace();
 			throw new ApplicationException(CODE_ENCRYPTION_FAILED, 
 					MessageFormat.format(MESSAGE_ENCRYPTION_FAILED, e.getLocalizedMessage()), e);
 		}
 	}
 	
 	/**
-	 * <b>DECRYPT</b> a message
+	 * <b>DECRYPT</b> a message using the DES encryption mecanism.
 	 * @param encryptedData the encrypted data
-	 * @return the data de-crypted 
+	 * @return the de-crypted result 
 	 * @throws ApplicationException thrown if the encryption fails
 	 */
 	public static String decryptMessage(String encryptedData) throws ApplicationException  {
 
-        try {
+		try {
 
 			Cipher cipher = getCipher();
 
-			// Build key using SecretKeySpec
-            if (originalKey == null) {
-            	originalKey = new SecretKeySpec(Arrays.copyOf(decodedKey, 16), TRANSFORMATION);
-            }
+			initializeSecrets();
 
-			cipher.init(Cipher.DECRYPT_MODE, originalKey);
-            byte[] cipherText = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
-            
-            return new String(cipherText);
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+			byte[] cipherText = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+			
+			return new String(cipherText).trim();
 
-        } catch (final Exception e) {
+		} catch (final Exception e) {
+			e.printStackTrace();
 			throw new ApplicationException(CODE_ENCRYPTION_FAILED, 
 					MessageFormat.format(MESSAGE_ENCRYPTION_FAILED, e.getLocalizedMessage()), e);
 		}
