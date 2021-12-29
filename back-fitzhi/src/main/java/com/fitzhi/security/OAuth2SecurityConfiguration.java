4eed3ef912855a657d325cfa302a536dbb16baed
frvidal <frederic.vidal@fitzhi.com>
2021-12-29 12:16:22 +0100

    [Back][Tweak] Removal of the debug mode for oauth

--- back-fitzhi/src/main/java/com/fitzhi/security/OAuth2SecurityConfiguration.java
+++ back-fitzhi/src/main/java/com/fitzhi/security/OAuth2SecurityConfiguration.java
@@ -21,7 +21,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
-@EnableWebSecurity (debug = true)
+@EnableWebSecurity
 @Order(101)
 public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
