4f8d7e069a6f69eddd0c63c449ad77666defbc4f
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-04-07 07:48:19 +0200

    [Back] Polish

--- back-fitzhi/src/test/java/com/fitzhi/bean/impl/StaffHandler/StaffHandlerOpenIdCreateStaffMemberTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/bean/impl/StaffHandler/StaffHandlerOpenIdCreateStaffMemberTest.java
@@ -16,8 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * Testing the method {@link StaffHandler#createStaffMember(com.fitzhi.data.internal.OpenIdToken)}
  * 
@@ -26,7 +24,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Slf4j
 public class StaffHandlerOpenIdCreateStaffMemberTest {
 
 	@Autowired
