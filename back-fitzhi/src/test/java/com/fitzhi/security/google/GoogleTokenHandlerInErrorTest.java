8ce41d9ccf3c0103a88512e38cf4ebda1db1c90b
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-04-17 11:52:17 +0200

    [Back][Fix] Tests due the creation of the GitHub token handler

--- back-fitzhi/src/test/java/com/fitzhi/security/google/GoogleTokenHandlerInErrorTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/security/google/GoogleTokenHandlerInErrorTest.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,7 +24,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(properties = { "referential.dir=nonExistingPath" }) 
 public class GoogleTokenHandlerInErrorTest {
 	
-	@Autowired 
+	@Autowired
+	@Qualifier("GOOGLE")
 	TokenHandler tokenHandler;
 
 	/**
--- back-fitzhi/src/test/java/com/fitzhi/security/google/GoogleTokenHandlerTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/security/google/GoogleTokenHandlerTest.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,7 +22,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GoogleTokenHandlerTest {
 	
-	@Autowired 
+	@Autowired
+	@Qualifier("GOOGLE")
 	TokenHandler tokenHandler;
 
 	/**
