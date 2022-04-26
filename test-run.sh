25af3986ad41f1481cee1219b0c642f230e85d02
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-04-26 19:30:25 +0200

    [Back] [Tweak] Oauth test

--- test-run.sh
+++ test-run.sh
@@ -1,6 +1,6 @@
 rm -rf deploy
 ./init.sh
 mv deploy/data/referential/openid-servers.json deploy/data/referential/openid-servers-empty.json
-mv deploy/data/referential/openid-servers-github-google.json deploy/data/referential/openid-servers.json
+mv deploy/data/referential/openid-servers-github-google-localhost-4200.json deploy/data/referential/openid-servers.json
 ./run.sh HTTP
 
