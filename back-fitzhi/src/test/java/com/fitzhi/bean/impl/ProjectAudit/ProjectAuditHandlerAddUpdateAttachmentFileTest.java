3aac0a60378bd74c43d82838e1c699fc584f34b9
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-05-05 20:51:43 +0200

    [Back] [Fix] Tests

--- back-fitzhi/src/test/java/com/fitzhi/bean/impl/ProjectAudit/ProjectAuditHandlerAddUpdateAttachmentFileTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/bean/impl/ProjectAudit/ProjectAuditHandlerAddUpdateAttachmentFileTest.java
@@ -45,7 +45,7 @@ public class ProjectAuditHandlerAddUpdateAttachmentFileTest {
 	private int ID_PROJECT = 314116;
 	@Before
 	public void before() throws ApplicationException {
-		project = projectHandler.addNewProject(new Project(ID_PROJECT, "PI"));
+		project = projectHandler.addNewProject(new Project(ID_PROJECT, "PI 2"));
 		project.setAudit(new HashMap<Integer, AuditTopic>());
 		AuditTopic auditTopic = new AuditTopic();
 		auditTopic.setEvaluation(40);
