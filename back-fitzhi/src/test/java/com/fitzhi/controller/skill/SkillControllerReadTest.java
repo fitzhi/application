fd86168826af1df0ccc30b064fb0b573837518cc
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-05-10 08:42:38 +0200

    [Back][Fix] DirtiesContext on test

--- back-fitzhi/src/test/java/com/fitzhi/controller/skill/SkillControllerReadTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/controller/skill/SkillControllerReadTest.java
@@ -25,6 +25,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -37,6 +39,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
 public class SkillControllerReadTest {
 
 	@Autowired
