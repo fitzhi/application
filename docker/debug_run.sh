2280e43bcc0952f6c2954772470cb901d9c96007
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-06-28 11:22:32 +0200

    [Docker] Container test

---
+++ docker/debug_run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export VERSION_FITZHI=`cat ../back-fitzhi/VERSION_FITZHI`
+export SONAR_HOST_URL=`cat ./SONAR_HOST_URL`
+export SONAR_TOKEN_LOGIN=`cat ./SONAR_TOKEN_LOGIN`
+
+
+# For debugging purpose only...
+docker run --name fitzhi  \
+  -e "organization=fitzhi" \
+  -e "urlSonarServer=${SONAR_HOST_URL}" \
+  -e "login=${SONAR_TOKEN_LOGIN}" \
+  -v fitzhi-data:/fitzhi/deploy/ \
+  -p 80:80 -ti --rm fitzhi/application:1.9-SNAPSHOT /bin/bash
