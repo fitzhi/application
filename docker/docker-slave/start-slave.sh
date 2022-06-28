a2e853fed35f4310806e700ba17e1bbc88e75650
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-06-28 21:25:34 +0200

    [Docker] Ending the container after completion

--- docker/docker-slave/start-slave.sh
+++ docker/docker-slave/start-slave.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 java -Xmx1g -jar fitzhi-1.9-SNAPSHOT.jar --spring.profiles.active="slave, HTTP" > out.txt
-
-# Terminating the container from inside.
-# cf. https://stackoverflow.com/questions/31538314/stopping-docker-container-from-inside
-trap "exit" SIGINT SIGTERM
-kill -s SIGINT 1.
