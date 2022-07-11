2865e6730844f3974283a1a7d530f8aa5fba5154
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-07-11 10:58:31 +0200

    [Back][Fix] Test

--- back-fitzhi/src/test/java/com/fitzhi/bean/impl/ProjectHandler/ProjectHandlerWithDataProdTest.java
+++ back-fitzhi/src/test/java/com/fitzhi/bean/impl/ProjectHandler/ProjectHandlerWithDataProdTest.java
@@ -12,6 +12,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -22,6 +24,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(properties = { "applicationOutDirectory=src/test/resources/data-prod/" }) 
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class ProjectHandlerWithDataProdTest {
 
 	@Autowired
