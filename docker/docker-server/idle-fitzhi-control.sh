4a04c3e0129c9657f2f82cff71c5f008f875fe80
Frédéric VIDAL <frederic.vidal@fitzhi.com>
2022-07-11 21:43:02 +0200

    [Docker] Fix idle control

--- docker/docker-server/idle-fitzhi-control.sh
+++ docker/docker-server/idle-fitzhi-control.sh
@@ -4,19 +4,20 @@ echo " * starting fitzhi idle control"
 
 MPHR=60    # Minutes per hour.
 
-sleep 5m
-stat -c '%Y' "./logs/spring-boot-logger.log"
-LOG_lastModified=$(date '+%s' -d "@$( stat -c '%Y' "./logs/spring-boot-logger.log"; )")
-echo $LOG_lastModified
+sleep 2m
 MINUTES=0
+
 while [[ $MINUTES < 10 ]]
 do
+  LOG_lastModified=$(date '+%s' -d "@$( stat -c '%Y' "./logs/spring-boot-logger.log"; )")
+  echo $LOG_lastModified
   NOW=$(date '+%s')
   echo $NOW
   MINUTES=$(( ($NOW - $LOG_lastModified) / $MPHR ))
   echo $MINUTES
   sleep 1m
 done
+
 kill $(cat ./pid.file)
-echo "Server termination!"
+echo " * fitzhi termination!"
 
